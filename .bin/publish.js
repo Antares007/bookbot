3a2698a70f9054237859e12c4566e9a4d4a7fa7c
Archil Bolkvadze <a.bolkvadze@gmail.com>
2019-01-14 20:49:18 +0400

    publish

--- .bin/publish.js
+++ .bin/publish.js
@@ -2,7 +2,9 @@
 const fs = require("fs")
 const path = require("path")
 const pkg = JSON.parse(
-  fs.readFileSync(path.join(__dirname, "package.json"), "utf8")
+  fs.readFileSync(path.join(__dirname, "..", "package.json"), "utf8")
 )
-console.log(process.argv[2])
+console.log(
+  fs.readFileSync(path.join(__dirname, "..", process.argv[2]), "utf8")
+)
 console.log(pkg)
