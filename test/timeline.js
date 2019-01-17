21a272001b795ab496aeb49fb0d5bc4ece5651b1
Archil Bolkvadze <a.bolkvadze@gmail.com>
2019-01-17 21:04:51 +0400

    add sample test

---
+++ test/timeline.js
@@ -0,0 +1,30 @@
+//@flow
+import { Ring, Test } from "../src/test.js"
+import { run } from "../src/test.js"
+
+console.log(
+  run(
+    Ring("timeline", o => () => {
+      console.log(0)
+      o(
+        Test("t1", done => {
+          console.log(1)
+          done()
+          console.log(2)
+
+          //
+        })
+      )
+      o(
+        Test("t1", done => {
+          console.log(3)
+          done()
+          console.log(4)
+
+          //
+        })
+      )
+      console.log(5)
+    })
+  )
+)
