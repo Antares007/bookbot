0aa7ccae0c89974e40f9c9f23332035461c08323
Archil Bolkvadze <a.bolkvadze@gmail.com>
2019-01-24 20:15:01 +0400

    simplify scheduler

--- src/scheduler.js
+++ src/scheduler.js
@@ -1,68 +1,74 @@
 //@flow
 export type Schedule = (
-  (Schedule | number, ?Schedule) => void,
+  o: (Schedule | number, ?Schedule) => void,
   t: number
 ) => void
 
-import * as tl from "../src/timeline.js"
+export function run(
+  now: number,
+  nextFrame: (f: (now: number) => void) => void,
+  pith: Schedule
+): void {
+  let line: Array<[number, Schedule]> = []
+  const append = (t, s) => {
+    const i = findAppendPosition(t, line)
+    if (i > -1 && line[i][0] === t) {
+      line[i][1] = mappend(line[i][1], s)
+    } else {
+      line.splice(i + 1, 0, [t, s])
+    }
+  }
+  const onNextFrame = now => {
+    while (true) {
+      const ap = findAppendPosition(now, line)
+      if (ap === -1) break
+      const line_ = line
+      line = ap === line.length - 1 ? [] : line.slice(ap + 1)
+      for (let i = 0; i <= ap; i++) runNow(append, line_[i][0], line_[i][1])
+    }
+    if (line.length > 0) nextFrame(onNextFrame)
+  }
+  runNow(append, now, pith)
+  if (line.length > 0) nextFrame(onNextFrame)
+}
 
-function run(o: (number, Schedule) => void, t: number, pith: Schedule): void {
+function findAppendPosition<T>(n: number, line: Array<[number, T]>): number {
+  var l = 0
+  var r = line.length
+  while (true) {
+    if (l < r) {
+      const m = ~~((l + r) / 2) | 0
+      if (line[m][0] > n) {
+        r = m
+        continue
+      } else {
+        l = m + 1
+        continue
+      }
+    } else {
+      return l - 1
+    }
+  }
+  throw new Error("never")
+}
+
+function runNow(
+  o: (number, Schedule) => void,
+  t: number,
+  pith: Schedule
+): void {
   pith((a, b) => {
     if (typeof a === "number") {
       if (b) o(t + a, b)
     } else {
-      run(o, t, a)
+      runNow(o, t, a)
     }
   }, t)
 }
 
-export function mappend(l: Schedule, r: Schedule): Schedule {
+function mappend(l: Schedule, r: Schedule): Schedule {
   return (o, t) => {
     l(o, t)
     r(o, t)
-  }
-}
-
-export function mkScheduler(
-  tf: () => number,
-  nextFrame: (f: () => void) => void
-): (s: Schedule) => void {
-  let current = null
-  let active = false
-  let now = -1
-  const add = timeline => {
-    if (current && timeline) {
-      current = tl.mappend(mappend, current, timeline)
-    } else if (timeline) {
-      current = timeline
-    }
-  }
-  const onNextFrame = () => {
-    if (!current) {
-      active = false
-      return
-    }
-    now = tf()
-    const cur = current
-    current = null
-    add(
-      tl.fromPith(mappend, o =>
-        tl.runTo(
-          v => run((t, pith) => o([t, pith]), v[0], v[1]),
-          mappend,
-          now,
-          cur
-        )
-      )
-    )
-    nextFrame(onNextFrame)
-  }
-  return pith => {
-    if (!active) {
-      nextFrame(onNextFrame)
-      active = true
-      now = tf()
-    }
-    add(tl.fromPith(mappend, o => run((t, s) => o([t, s]), now, pith)))
   }
 }
--- test/scheduler.js
+++ test/scheduler.js
@@ -3,30 +3,33 @@ import type { A, Test } from "../src/atest.js"
 import * as s from "../src/scheduler.js"
 
 export function t(assert: A & Test): void {
-  const actual = { t: [30, 60, 90, 99], l: [], i: 0 }
-  const scheduler = s.mkScheduler(
-    () => actual.t.shift(),
-    f => (Promise.resolve().then(() => (actual.i++, f())), void 0)
-  )
+  const actual = { t: [30, 99], l: [], i: 0 }
   const expected = {
-    i: 2,
-    t: [99],
-    l: [[0, 30, 0], [1, 30, 0], [3, 60, 1], [2, 90, 2], [4, 90, 2]]
+    i: 1,
+    t: [],
+    l: [[0, 30, 0], [1, 30, 0], [3, 60, 1], [2, 90, 1], [4, 90, 1]]
   }
-  scheduler((o, t) => {
-    actual.l.push([0, t, actual.i])
-    o((o, t) => {
-      actual.l.push([1, t, actual.i])
-      o(60, (o, t) => {
-        actual.l.push([2, t, actual.i])
+  s.run(
+    actual.t.shift(),
+    f => {
+      actual.i++
+      f(actual.t.shift())
+    },
+    (o, t) => {
+      actual.l.push([0, t, actual.i])
+      o((o, t) => {
+        actual.l.push([1, t, actual.i])
+        o(60, (o, t) => {
+          actual.l.push([2, t, actual.i])
+        })
       })
-    })
-    o(30, (o, t) => {
-      actual.l.push([3, t, actual.i])
       o(30, (o, t) => {
-        actual.l.push([4, t, actual.i])
-        assert.deepStrictEqual(actual, expected)
+        actual.l.push([3, t, actual.i])
+        o(30, (o, t) => {
+          actual.l.push([4, t, actual.i])
+          assert.deepStrictEqual(actual, expected)
+        })
       })
-    })
-  })
+    }
+  )
 }
