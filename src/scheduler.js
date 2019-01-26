e992242488f24a449cf47453220328c450bdbdda
Archil Bolkvadze <a.bolkvadze@gmail.com>
2019-01-26 12:25:55 +0400

    change scheduler iface

--- src/scheduler.js
+++ src/scheduler.js
@@ -4,7 +4,7 @@ export type Schedule = (time: number) => void
 
 export function mkRun<H>(
   tf: () => number,
-  setRunat: (f: () => void, runAt: number) => H
+  setTimeout: (f: () => void, delay: number) => H
 ): O {
   let currentTime: number = -1
   let line: Array<[number, Schedule]> = []
@@ -20,26 +20,27 @@ export function mkRun<H>(
       line.splice(i + 1, 0, [t, s])
     }
   }
-  const atNextRun = () => {
+  const onTimeout = () => {
     if (line.length === 0) {
       currentTime = -1
       return
     }
-    const ap = findAppendPosition(currentTime, line)
-    if (ap > -1) {
+    currentTime = tf()
+    while (true) {
+      const ap = findAppendPosition(currentTime, line)
+      if (ap === -1) break
       const line_ = line
       line = ap === line.length - 1 ? [] : line.slice(ap + 1)
       for (let i = 0; i <= ap; i++) {
         line_[i][1](line_[i][0])
       }
     }
-    currentTime = line.length > 0 ? line[0][0] : currentTime
-    setRunat(atNextRun, currentTime)
+    setTimeout(onTimeout, line.length > 0 ? Math.max(0, line[0][0] - tf()) : 0)
   }
   return (a, b) => {
     if (currentTime === -1) {
       currentTime = tf()
-      setRunat(atNextRun, currentTime)
+      setTimeout(onTimeout, 0)
     }
     if (typeof a === "number" && typeof b === "function") {
       append(currentTime + a, b)
--- test/scheduler.js
+++ test/scheduler.js
@@ -7,11 +7,62 @@ export function t(assert: A & Test): void {
   const expected = {
     frame: 3,
     t: [30, 60, 90],
-    l: [["A", 30, 0], ["B", 30, 0], ["C", 60, 2], ["D", 90, 3], ["E", 90, 3]]
+    l: [[30, 0], [30, 0], [60, 2], [90, 3], [90, 3]]
   }
-  const so = s.mkRun(
-    () => 30,
+  let ctime = 30
+  const oS = s.mkRun(
+    () => ctime,
+    (f, delay) => {
+      ctime += delay
+      actual.t.push(ctime)
+      return Promise.resolve().then(() => {
+        actual.frame++
+        f()
+      })
+    }
+  )
+  const o = (a, b) => {
+    const pushTime = p => t => {
+      actual.l.push([t, actual.frame])
+      p(t)
+    }
+    if (typeof a === "number" && typeof b === "function") {
+      oS(a, pushTime(b))
+    } else {
+      if (typeof a === "function") oS(pushTime(a))
+      if (typeof b === "function") oS(pushTime(b))
+    }
+  }
+
+  o(t => {
+    o(t => {
+      o(60, t => {})
+    })
+    o(30, t => {
+      o(
+        30,
+        assert(t => {
+          assert.deepStrictEqual(actual, expected)
+        })
+      )
+    })
+  })
+  assert.ok(true)
+}
+
+export function t2(assert: A & Test): void {
+  const actual = { t: [], l: [], frame: 0 }
+  const expected = {
+    frame: 1,
+    t: [0],
+    l: [[30, 0], [30, 1], [30, 1]]
+  }
+
+  let ctime = 30
+  const oS = s.mkRun(
+    () => ctime,
     (f, at) => {
+      ctime += at
       actual.t.push(at)
       return Promise.resolve().then(() => {
         actual.frame++
@@ -19,20 +70,32 @@ export function t(assert: A & Test): void {
       })
     }
   )
-  so(t => {
-    actual.l.push(["A", t, actual.frame])
-    so(t => {
-      actual.l.push(["B", t, actual.frame])
-      so(60, t => {
-        actual.l.push(["D", t, actual.frame])
-      })
+  const o = (a, b) => {
+    const pushTime = p => t => {
+      actual.l.push([t, actual.frame])
+      p(t)
+    }
+    if (typeof a === "number" && typeof b === "function") {
+      oS(a, pushTime(b))
+    } else {
+      if (typeof a === "function") oS(pushTime(a))
+      if (typeof b === "function") oS(pushTime(b))
+    }
+  }
+
+  o(t => {
+    o(0, t => {
+      o(
+        0,
+        assert(t => {
+          assert.deepStrictEqual(actual, expected)
+        })
+      )
     })
-    so(30, t => {
-      actual.l.push(["C", t, actual.frame])
-      so(30, t => {
-        actual.l.push(["E", t, actual.frame])
-        assert.deepStrictEqual(actual, expected)
-      })
-    })
+  })
+  assert.deepStrictEqual(actual, {
+    frame: 0,
+    t: [0],
+    l: [[30, 0]]
   })
 }
