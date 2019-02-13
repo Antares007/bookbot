b51491db8e6e46afb0edb3f0f3a24ee5bae16e0f
Archil Bolkvadze <a.bolkvadze@gmail.com>
2019-02-13 21:12:50 +0400

    reshape atest

---
+++ src/atest/index.js
@@ -0,0 +1,80 @@
+// @flow strict
+import { parse } from './parse'
+import { readFileSync } from 'fs'
+import { join } from 'path'
+
+export type A = {
+  +ok: <A>(value: A, message?: string) => void,
+  +strictEqual: <A, B>(actual: A, expected: B, message?: string) => void,
+  +deepStrictEqual: <A, B>(actual: A, expected: B, message?: string) => void,
+  +notStrictEqual: <A, B>(actual: A, expected: B, message?: string) => void,
+  +notDeepStrictEqual: <A, B>(actual: A, expected: B, message?: string) => void
+}
+const assert = require.call(module, 'assert').strict
+
+export function run(
+  o: ({ fullname: string, error?: Error }) => void,
+  filePaths: Iterable<string>
+): void {
+  for (let filePath of filePaths) {
+    try {
+      const testDeclarations = parse(readFileSync(filePath, 'utf8'))
+      if (testDeclarations.length === 0) continue
+      const exports = require.call(module, filePath)
+      let timeoutID
+      let totalPlan = 0
+      const plan: { [string]: number } = testDeclarations.reduce((a, d) => {
+        const fullname = join(filePath, d.name)
+        a[fullname] = d.plan
+        totalPlan += d.plan
+        runTest(error => {
+          if (error != null) o({ fullname, error })
+          else {
+            if (--a[fullname] === 0) o({ fullname })
+            if (--totalPlan === 0) clearTimeout(timeoutID)
+          }
+        }, exports[d.name])
+        return a
+      }, {})
+      timeoutID = setTimeout(() => {
+        for (let name in plan)
+          if (plan[name] !== 0)
+            o({ fullname: name, error: new Error('timeout') })
+      }, 1000)
+    } catch (error) {
+      o({ fullname: filePath, error })
+    }
+  }
+}
+
+function runTest(o: (?Error) => void, f): void {
+  var i = 0
+  try {
+    const p = f.call(
+      {},
+      new Proxy([], {
+        get(_, property) {
+          const index = parseInt(property, 10)
+          if (isNaN(index)) return Reflect.get(...arguments)
+          return new Proxy(assert, {
+            get(_, property) {
+              return (...args) => {
+                try {
+                  assert[property](...args)
+                  if (i === index) o()
+                  else o(new Error(`index actual(${i}) === expected(${index})`))
+                  i++
+                } catch (err) {
+                  o(err)
+                }
+              }
+            }
+          })
+        }
+      })
+    )
+    if (p instanceof Promise) p.catch(o)
+  } catch (err) {
+    o(err)
+  }
+}
--- test/test.js
+++ test/test.js
@@ -1,55 +1,14 @@
 //@flow strict
 import type { A } from '../src/atest'
-import { awaitPromises } from '../src/iterable'
 import { run } from '../src/atest'
-import * as path from 'path'
+import { join } from 'path'
 
-const fail = '\u001b[31mfail\u001b[39m'
-const pass = '\u001b[32mpass\u001b[39m'
-
-const rez = [0, 0]
-awaitPromises(
-  v => {
-    if (v == null) {
-      console.log(pass, rez[0])
-      console.log(fail, rez[1])
-    } else if (v instanceof Error) {
-      throw v
-    } else {
-      const e = v.error
-      if (e != null) {
-        rez[1]++
-        console.log()
-        console.group(`${v.name}(${v.time})`, fail)
-        console.log(e.message)
-        console.log(
-          e.stack
-            .split(/\n/gi)
-            .filter(l => l.includes(path.join(__dirname, '..')))
-            .map(l =>
-              l
-                .trim()
-                .split(path.join(__dirname, '..') + '/')
-                .join('./')
-            )
-            .join('\n')
-        )
-        console.groupEnd()
-        console.log()
-      } else {
-        rez[0]++
-        process.stdout.write('.')
-      }
-    }
-  },
-  run(
-    __dirname,
-    {
-      ...require('fs'),
-      ...require('assert'),
-      ...path,
-      require: s => require.call(module, s)
-    }
-    //n => n.includes('schedu')
-  )
-)
+const tests = ['scheduler.js', 'stream.js']
+run(({ fullname, error }) => {
+  if (!error) process.stdout.write('.')
+  else {
+    console.group(fullname.slice(__dirname.length))
+    console.log(error)
+    console.groupEnd()
+  }
+}, tests.map(a => join(__dirname, a)))
