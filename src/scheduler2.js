d0fdc17c07a9211c755220876ab9b9cad5519f7f
Archil Bolkvadze <a.bolkvadze@gmail.com>
2019-02-25 13:41:09 +0400

    sync

--- src/scheduler2.js
+++ src/scheduler2.js
@@ -1,25 +1,31 @@
 // @flow strict
 import type { Disposable } from './disposable'
 
-export opaque type Scheduler$Pith = (
-  (Scheduler$O) => void,
-  number
-) => ?Disposable
-export opaque type Scheduler$O =
-  | { type: 'now', pith: Scheduler$Pith }
-  | { type: 'delay', delay: number, pith: Scheduler$Pith }
+export opaque type TimePoint: number = number
 
-export const now = (pith: Scheduler$Pith): Scheduler$O => ({
-  type: 'now',
-  pith
-})
+export type Scheduler$Pith = (
+  (Scheduler$O) => void,
+  () => TimePoint
+) => ?Disposable
+
+export type Scheduler$O = {
+  type: 'delay',
+  delay: number,
+  pith: Scheduler$Pith
+}
+
 export const delay = (delay: number, pith: Scheduler$Pith): Scheduler$O => ({
   type: 'delay',
   delay,
   pith
 })
 
-export function run(i: Scheduler$O): ?Disposable {
-  if (i.type === 'now') return
+export function run(v: Scheduler$O): Disposable {
   return { dispose() {} }
 }
+
+let see = run(
+  delay(0, (o, now) => {
+    o(delay(0, (o, now) => {}))
+  })
+)
--- src/stream2.js
+++ src/stream2.js
@@ -1,18 +1,65 @@
-// flow strict
+// @flow strict
 
-export type stream$O<A> =
-  | { type: 'event', t: TimePoint, v: A }
-  | { type: 'end', t: TimePoint }
-  | { type: 'error', t: TimePoint, v: Error }
+import type { Disposable } from './disposable'
+import type { TimePoint, Scheduler$O } from './scheduler2'
+import { delay } from './scheduler2'
 
-type SFn<A> = ((stream$O<A>) => void, Scheduler) => Disposable
+type S$Pith<A> = ((S$O<A> | Scheduler$O) => void) => Disposable
+type S<A> = { type: 'stream', pith: S$Pith<A> }
 
-export function event<A>(t: TimePoint, v: A): stream$O<A> {
-  return { type: 'event', t, v }
+export type S$O<A> =
+  | { type: 'event', v: A }
+  | { type: 'end' }
+  | { type: 'error', v: Error }
+
+export const event = <A>(t: TimePoint, v: A): S$O<A> => ({
+  type: 'event',
+  t,
+  v
+})
+export const end = <A>(t: TimePoint): S$O<A> => ({ type: 'end', t })
+export const error = <A>(t: TimePoint, v: Error): S$O<A> => ({
+  type: 'error',
+  t,
+  v
+})
+export { delay }
+
+export const run = <A>(
+  o: (S$O<A> & { t: TimePoint }) => void,
+  v: S<A>
+): Scheduler$O => {
+  return delay(0, (oS, now) => {
+    let see = v.pith(e => {
+      if (e.type === 'event') {
+        var see = Object.assign({}, e, { t: now() })
+        o(see)
+      }
+    })
+  })
 }
-export function end<A>(t: TimePoint): stream$O<A> {
-  return { type: 'end', t }
-}
-export function error<A>(t: TimePoint, v: Error): stream$O<A> {
-  return { type: 'error', t, v }
-}
+
+export const Of = <A>(pith: S$Pith<A>): S<A> => ({
+  type: 'stream',
+  pith
+})
+export const at = <A>(a: A, delay_: number = 0): S<A> =>
+  Of(o => {
+    return { dispose() {} }
+  })
+
+// {
+//
+//   return new S((o, schdlr) => {
+//     var active = true
+//     schdlr.delay(delay_, t => {
+//       if (active) o(event(t, a))
+//       if (active) o(end(t))
+//     })
+//     return {
+//       dispose() {
+//         active = false
+//       }
+//     }
+//   })
+// }
