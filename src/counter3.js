c1d2e552d1b939b282c20f88bc85626fcf8a4d80
Archil Bolkvadze <a.bolkvadze@gmail.com>
2019-03-05 05:41:44 +0400

    use classes

--- src/counter3.js
+++ src/counter3.js
@@ -1,25 +1,24 @@
 // @flow strict
 import * as S from './stream2'
-import type { PNode$pith } from './pnode2'
 import * as N from './pnode2'
 import { now } from './scheduler2'
 
-const elm = (tag: string, xpith: S.S<PNode$pith> | PNode$pith) =>
-  N.node(
+const elm = <A>(tag: string, xpith: S.S<N.Pith<A>> | N.Pith<A>): N.PNode<A> =>
+  N.PNode.of(
     () => document.createElement(tag),
     n => n.nodeName === tag.toUpperCase(),
     xpith instanceof S.S ? xpith : S.at(xpith)
   )
-const text = (stext: S.S<string>) =>
-  N.node(
+const text = <A>(stext: S.S<string>): N.PNode<A> =>
+  N.PNode.of(
     () => document.createTextNode(''),
     n => n.nodeName === '#text',
     S.at(
-      N.pith(o =>
+      N.Pith.of(o =>
         o(
           S.map(
             text =>
-              N.patch(n => {
+              N.Patch.of(n => {
                 n.textContent = text
               }),
             stext
@@ -32,53 +31,23 @@ const text = (stext: S.S<string>) =>
 const counter = (d: number) =>
   elm(
     'div',
-    N.pith(o => {
+    N.Pith.of(o => {
+      o(S.at(1))
       o(
         elm(
           'button',
-          N.pith(o => {
+          N.Pith.of(o => {
             o(text(S.at('+')))
-            //     d > 0 && o(counter(d - 1))
-            o(
-              elm(
-                'div',
-                N.pith(o => {
-                  o(
-                    elm(
-                      'button',
-                      N.pith(o => {
-                        o(
-                          text(
-                            S.periodic(1000)
-                              .take(10)
-                              .map(i => i + ' +')
-                          )
-                        )
-                        //     d > 0 && o(counter(d - 1))
-                      })
-                    )
-                  )
-                  o(
-                    elm(
-                      'button',
-                      N.pith(o => {
-                        o(text(S.at('-')))
-                        //    d > 0 && o(counter(d - 1))
-                      })
-                    )
-                  )
-                })
-              )
-            )
+            d > 0 && o(counter(d - 1))
           })
         )
       )
       o(
         elm(
           'button',
-          N.pith(o => {
+          N.Pith.of(o => {
             o(text(S.at('-')))
-            //    d > 0 && o(counter(d - 1))
+            d > 0 && o(counter(d - 1))
           })
         )
       )
@@ -89,8 +58,10 @@ const rootNode = document.getElementById('root-node')
 if (!rootNode) throw new Error('cant find root-node')
 
 const t0 = now()
-N.run(S.at(N.pith(o => o(counter(0)))))
-  .map(p => (p(rootNode), 1))
+N.run(S.at(N.Pith.of(o => o(counter(3)))))
+  .map(p => {
+    return p instanceof N.Patch ? (p.v(rootNode), 1) : 0
+  })
   .scan((a, b) => a + b, 0)
   .skip(1)
   .map(n => ({ n, t: now() - t0 }))
--- src/pnode2.js
+++ src/pnode2.js
@@ -1,41 +1,49 @@
 // @flow strict
 import * as S from './stream2'
 
-export type PNode$patch = { t: 'PNode$patch', patch: Node => void }
-export const patch = (
-  patch: $PropertyType<PNode$patch, 'patch'>
-): PNode$patch => ({
-  t: 'PNode$patch',
-  patch
-})
-
-export type PNode$pith = {
-  t: 'PNode$pith',
-  pith: ((PNode$node | S.S<PNode$patch>) => void) => void
+export class Patch {
+  v: Node => void
+  constructor(v: $PropertyType<Patch, 'v'>) {
+    this.v = v
+  }
+  static of(v: $PropertyType<Patch, 'v'>) {
+    return new Patch(v)
+  }
 }
-export const pith = (pith: $PropertyType<PNode$pith, 'pith'>): PNode$pith => ({
-  t: 'PNode$pith',
-  pith
-})
 
-export type PNode$node = {
-  t: 'PNode$node',
-  create: () => Node,
-  eq: Node => boolean,
-  spith: S.S<PNode$pith>
+export class Pith<A> {
+  pith: ((PNode<A> | S.S<A>) => void) => void
+  constructor(v: $PropertyType<Pith<A>, 'pith'>) {
+    this.pith = v
+  }
+  static of(v: $PropertyType<Pith<A>, 'pith'>) {
+    return new Pith(v)
+  }
 }
-export const node = (
-  create: $PropertyType<PNode$node, 'create'>,
-  eq: $PropertyType<PNode$node, 'eq'>,
-  spith: $PropertyType<PNode$node, 'spith'>
-): PNode$node => ({
-  t: 'PNode$node',
-  create,
-  eq,
-  spith
-})
 
-export function run(spith: S.S<PNode$pith>): S.S<(Node) => void> {
+export class PNode<A> {
+  create: () => Node
+  eq: Node => boolean
+  spith: S.S<Pith<A>>
+  constructor(
+    create: $PropertyType<PNode<A>, 'create'>,
+    eq: $PropertyType<PNode<A>, 'eq'>,
+    spith: $PropertyType<PNode<A>, 'spith'>
+  ) {
+    this.create = create
+    this.eq = eq
+    this.spith = spith
+  }
+  static of(
+    create: $PropertyType<PNode<A>, 'create'>,
+    eq: $PropertyType<PNode<A>, 'eq'>,
+    spith: $PropertyType<PNode<A>, 'spith'>
+  ) {
+    return new PNode(create, eq, spith)
+  }
+}
+
+export function run<A>(spith: S.S<Pith<A>>): S.S<Patch | A> {
   return S.switchLatest(
     spith.map(x => {
       var pnodesCount = 0
@@ -43,39 +51,45 @@ export function run(spith: S.S<PNode$pith>): S.S<(Node) => void> {
       var pnodes: Array<[number, () => Node, (Node) => boolean]> = []
       x.pith(x => {
         if (x instanceof S.S) {
-          p = p.merge(x.map(p => p.patch))
+          p = p.merge(x)
         } else {
           const index = pnodesCount++
           pnodes.push([index, x.create, x.eq])
           p = p.merge(
-            S.map(p => parent => p(parent.childNodes[index]), run(x.spith))
+            run(x.spith).map(x =>
+              x instanceof Patch
+                ? Patch.of(parent => x.v(parent.childNodes[index]))
+                : x
+            )
           )
         }
       })
       if (pnodesCount === 0) return p
-      return p.startWith((parent: Node) => {
-        const childNodes = parent.childNodes
-        const misplacedPnodes: Array<[Node, Node]> = []
-        for (var i = 0; i < childNodes.length && pnodes.length > 0; i++) {
-          const li = childNodes[i]
-          for (var j = 0; j < pnodes.length; j++) {
-            const pi = pnodes[j]
-            if (pi[2](li)) {
-              pnodes.splice(j, 1)
-              if (pi[0] !== i) misplacedPnodes.push([li, childNodes[pi[0]]])
-              break
+      return p.startWith(
+        Patch.of(parent => {
+          const childNodes = parent.childNodes
+          const misplacedPnodes: Array<[Node, Node]> = []
+          for (var i = 0; i < childNodes.length && pnodes.length > 0; i++) {
+            const li = childNodes[i]
+            for (var j = 0; j < pnodes.length; j++) {
+              const pi = pnodes[j]
+              if (pi[2](li)) {
+                pnodes.splice(j, 1)
+                if (pi[0] !== i) misplacedPnodes.push([li, childNodes[pi[0]]])
+                break
+              }
             }
           }
-        }
-        for (var pm of misplacedPnodes) parent.replaceChild(pm[0], pm[1])
-        for (var pn of pnodes) {
-          const oldNode = childNodes[pn[0]]
-          if (oldNode) parent.replaceChild(pn[1](), oldNode)
-          else parent.insertBefore(pn[1](), null)
-        }
-        for (var i = childNodes.length - 1; i >= pnodesCount; i--)
-          parent.removeChild(childNodes[i])
-      })
+          for (var pm of misplacedPnodes) parent.replaceChild(pm[0], pm[1])
+          for (var pn of pnodes) {
+            const oldNode = childNodes[pn[0]]
+            if (oldNode) parent.replaceChild(pn[1](), oldNode)
+            else parent.insertBefore(pn[1](), null)
+          }
+          for (var i = childNodes.length - 1; i >= pnodesCount; i--)
+            parent.removeChild(childNodes[i])
+        })
+      )
     })
   )
 }
