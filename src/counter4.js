a12cc22bbd22144474e7d1466cd5f946e3e4b79b
Archil Bolkvadze <a.bolkvadze@gmail.com>
2019-03-07 15:11:09 +0400

    sync

--- src/counter4.js
+++ src/counter4.js
@@ -4,35 +4,32 @@ import * as P from './pnode2'
 import * as D from './dom2'
 import { now, delay } from './scheduler2.js'
 
+const btn = (a, pith) => D.elm('button', [D.action({ click: e => a })], pith)
 const Counter = (d: number) =>
   D.elm('div', [], (o, acts) => {
     o(
-      D.elm('button', [D.action('click', e => +1)], o => {
+      D.elm('button', [D.action({ click: e => +1 })], o => {
         o(D.str('+'))
         d > 0 && o(Counter(d - 1))
       })
     )
     o(
-      D.elm('button', [D.action('click', e => -1)], o => {
+      D.elm('button', [D.action({ click: e => -1 })], o => {
         o(D.str('-'))
         d > 0 && o(Counter(d - 1))
       })
     )
     o(
-      D.elm('div', [], o =>
-        o(D.str(acts.scan((a, b) => a + b, 0).map(n => String(n))))
-      )
+      D.elm('div', [], o => o(D.str(acts.scan((a, b) => a + b, 0).map(String))))
     )
   })
 
 const rootNode = document.getElementById('root-node')
 if (!rootNode) throw new Error('cant find root-node')
 
-const t0 = now()
 D.run(D.pith(o => o(Counter(1))))
-  .filter2(x => (x instanceof P.Patch ? x : null))
   .map(p => p.v(rootNode))
-  .scan(a => a + 1, 0)
-  .skip(1)
-  .map(n => ({ n, t: now() - t0 }))
-  .run(console.log.bind(console))
+  .run(e => {
+    if (e instanceof Error) console.error(e)
+    else if (e instanceof S.End) console.log(e)
+  })
--- src/dom2.js
+++ src/dom2.js
@@ -5,7 +5,7 @@ import * as P from './pnode2'
 type SS<A> = S.S<A> | A
 
 export class Attr {
-  map: { [string]: string }
+  map: { [string]: ?string }
   constructor(map: $PropertyType<Attr, 'map'>) {
     this.map = map
   }
@@ -15,7 +15,7 @@ export function attr(map: $PropertyType<Attr, 'map'>): Attr {
 }
 
 export class Style {
-  map: { [string]: string }
+  map: { [$Keys<CSSStyleDeclaration>]: ?string }
   constructor(map: $PropertyType<Style, 'map'>) {
     this.map = map
   }
@@ -25,29 +25,21 @@ export function style(map: $PropertyType<Style, 'map'>): Style {
 }
 
 export class Action<A> {
-  name: MouseEventTypes
-  toAction: Event => A
-  constructor(
-    name: $PropertyType<Action<A>, 'name'>,
-    toAction: $PropertyType<Action<A>, 'toAction'>
-  ) {
-    this.name = name
-    this.toAction = toAction
+  map: { [MouseEventTypes | string]: ?(Event) => A }
+  constructor(map: $PropertyType<Action<A>, 'map'>) {
+    this.map = map
   }
 }
-export function action<A>(
-  name: $PropertyType<Action<A>, 'name'>,
-  toAction: $PropertyType<Action<A>, 'toAction'>
-): Action<A> {
-  return new Action(name, toAction)
+export function action<A>(map: $PropertyType<Action<A>, 'map'>): Action<A> {
+  return new Action(map)
 }
 
 export class Elm<A> {
   tag: string
   key: ?string
-  attrs: S.S<Attr>
-  styles: S.S<Style>
-  actions: S.S<Action<A>>
+  attrs: ?S.S<Attr>
+  styles: ?S.S<Style>
+  actions: ?S.S<Action<A>>
   piths: S.S<Pith<A>>
   constructor(
     tag: $PropertyType<Elm<A>, 'tag'>,
@@ -67,21 +59,62 @@ export class Elm<A> {
 }
 export function elm<A>(
   tag: string,
-  attrs: Array<SS<Attr | Style | Action<A>>>,
+  attrsList: Array<SS<Attr | Style | Action<A>>>,
   piths: SS<$PropertyType<Pith<A>, 'pith'> | Pith<A>>,
   key?: string
 ): Elm<A> {
-  var s = S.empty()
-  for (var x of attrs) s = S.merge(s, x instanceof S.S ? x : S.at(x))
+  const piths_ = (piths instanceof S.S ? piths : S.at(piths)).map(x =>
+    x instanceof Pith ? x : pith(x)
+  )
+  if (attrsList.length === 0)
+    return new Elm<A>(tag, key, null, null, null, piths_)
+
+  var s: ?S.S<Attr | Style | Action<A>>
+  var attrMap: $PropertyType<Attr, 'map'> = {}
+  var attrs: ?S.S<Attr>
+  var styleMap: $PropertyType<Style, 'map'> = {}
+  var styles: ?S.S<Style>
+  var actionMap: $PropertyType<Action<A>, 'map'> = {}
+  var actions: ?S.S<Action<A>>
+  for (var x of attrsList) {
+    if (x instanceof S.S) {
+      s = s ? S.merge(s, x) : x
+    } else if (x instanceof Attr) {
+      attrMap = { ...attrMap, ...x.map }
+      if (!attrs) attrs = S.at(attr({}))
+    } else if (x instanceof Style) {
+      styleMap = { ...styleMap, ...x.map }
+      if (!styles) styles = S.at(style({}))
+    } else {
+      actionMap = { ...actionMap, ...x.map }
+      if (!actions) actions = S.at(action({}))
+    }
+  }
+  attrs =
+    attrs && s
+      ? attrs.merge(s.filter2(x => (x instanceof Attr ? x : null)))
+      : s != null
+      ? s.filter2(x => (x instanceof Attr ? x : null))
+      : attrs
+  styles =
+    styles && s
+      ? styles.merge(s.filter2(x => (x instanceof Style ? x : null)))
+      : s != null
+      ? s.filter2(x => (x instanceof Style ? x : null))
+      : styles
+  actions =
+    actions && s
+      ? actions.merge(s.filter2(x => (x instanceof Action ? x : null)))
+      : s != null
+      ? s.filter2(x => (x instanceof Action ? x : null))
+      : actions
   return new Elm<A>(
     tag,
     key,
-    s.filter2(x => (x instanceof Attr ? x : null)),
-    s.filter2(x => (x instanceof Style ? x : null)),
-    s.filter2(x => (x instanceof Action ? x : null)),
-    (piths instanceof S.S ? piths : S.at(piths)).map(x =>
-      x instanceof Pith ? x : pith(x)
-    )
+    attrs ? attrs.map(x => attr({ ...x.map, ...attrMap })) : null,
+    styles ? styles.map(x => style({ ...x.map, ...styleMap })) : null,
+    actions ? actions.map(x => action({ ...x.map, ...actionMap })) : null,
+    piths_
   )
 }
 
@@ -127,9 +160,52 @@ export function run<A>(piths: SS<Pith<A>>): S.S<P.Patch> {
         } else {
           o(
             P.pnode(
-              () => document.createElement(v.tag),
-              n => n.nodeName === v.tag,
-              v.piths.map(ring)
+              () => {
+                const elm = document.createElement(v.tag)
+                if (v.key) elm.dataset.key = v.key
+                return elm
+              },
+              n =>
+                n.nodeName === v.tag &&
+                (v.key == null ||
+                  (n instanceof HTMLElement && n.dataset.key === v.key)),
+              v.piths.map(ring).map(pith => o => {
+                if (v.actions) {
+                  var actionMap = {}
+                  const mkListener = (
+                    toAction: (e: Event) => A
+                  ): (Event => void) & { toAction(Event): A } => {
+                    handler.toAction = toAction
+                    return handler
+                    function handler(e: Event) {
+                      console.log('Event', handler.toAction(e))
+                    }
+                  }
+                  o(
+                    v.actions.map(({ map }) =>
+                      P.patch(node => {
+                        for (var key in actionMap)
+                          if (map[key] == null) {
+                            node.removeEventListener(key, actionMap[key])
+                            delete actionMap[key]
+                          }
+                        for (var key in map)
+                          if (actionMap[key] == null && map[key] != null) {
+                            const l = mkListener(map[key])
+                            node.addEventListener(key, l)
+                            actionMap[key] = l
+                          } else if (
+                            actionMap[key] != null &&
+                            map[key] != null
+                          ) {
+                            actionMap[key].toAction = map[key]
+                          }
+                      })
+                    )
+                  )
+                }
+                pith.pith(o)
+              })
             )
           )
         }
