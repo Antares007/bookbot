673438b8e44286457c034ee9fa2732064e290b0e
Archil Bolkvadze <a.bolkvadze@gmail.com>
2019-03-10 22:37:26 +0400

    fix sd

--- src/sd.js
+++ src/sd.js
@@ -1,10 +1,8 @@
-// flow strict
+// @flow strict
 import * as S from './stream'
 import * as D from './dom'
 import * as P from './pnode'
 import { On } from './on'
-
-type O<A> = D.DElm<A> | D.DStr | S.S<SDR<A> | P.PPatch>
 
 // export function omap<A, B>(
 //   f: A => B,
@@ -22,23 +20,16 @@ type O<A> = D.DElm<A> | D.DStr | S.S<SDR<A> | P.PPatch>
 //   }
 // }
 
-D.elm('div', (o, on) => {
-  on.click()
-})
-
-export class SDR<A> {
+export class R<A> {
   r: A => A
-  constructor(r: $PropertyType<SDR<A>, 'r'>) {
+  constructor(r: $PropertyType<R<A>, 'r'>) {
     this.r = r
   }
 }
-export function r<A>(r: $PropertyType<SDR<A>, 'r'>): SDR<A> {
-  return new SDR(r)
-}
+export class Elm {}
 
-// pith: ((DElm<A> | DStr | S.S<A | P.PPatch>) => void, On) => void
 export class SDPith<A> {
-  pith: ((D.DElm<A> | D.DStr | S.S<SDR<A>>) => void, On) => void
+  pith: ((D.Elm<A> | D.Str | S.S<R<A>>) => void, On) => void
   constructor(pith: $PropertyType<SDPith<A>, 'pith'>) {
     this.pith = pith
   }
