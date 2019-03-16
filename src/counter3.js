5a4b4da349ecb0efcc18bd8676df974e635715ff
Archil Bolkvadze <a.bolkvadze@gmail.com>
2019-03-16 14:24:01 +0400

    N

--- src/counter3.js
+++ src/counter3.js
@@ -1,37 +1,20 @@
-// flow strict-local
+// @flow strict
 import * as S from './stream'
-import * as P from './pnode'
+import * as N from './n'
 import * as M from './m'
 import { now, delay } from './scheduler'
 
-const elm = (tag: string, pith) =>
-  P.node(
-    () => document.createElement(tag),
-    n => n.nodeName === tag.toUpperCase(),
-    P.bark(pith)
-  )
-const text = (stext: S.S<string>) =>
-  P.node(
-    () => document.createTextNode(''),
-    n => n.nodeName === '#text',
-    stext.map(text =>
-      P.patch(n => {
-        n.textContent = text
-      })
-    )
-  )
-
 const counter = (d: number) =>
-  elm('div', o => {
+  N.elm('div', o => {
     o(
-      elm('button', o => {
-        o(text(S.at('+')))
+      N.elm('button', o => {
+        o(N.text(S.at('+')))
         d > 0 && o(counter(d - 1))
       })
     )
     o(
-      elm('button', o => {
-        o(text(S.at('-')))
+      N.elm('button', o => {
+        o(N.text(S.at('-')))
         d > 0 && o(counter(d - 1))
       })
     )
@@ -40,14 +23,15 @@ const counter = (d: number) =>
 const rootNode = document.getElementById('root-node')
 if (!rootNode) throw new Error('cant find root-node')
 const patches = []
-P.bark(o => o(counter(3))).run(e => {
+const rez = counter(3)
+rez.patches.run(e => {
   if (e instanceof Error) throw e
   else if (e instanceof S.End) {
     const t0 = now()
     const run = () => {
       const p = patches.shift()
       if (p) {
-        p.value.v(rootNode)
+        p.value.patch(rootNode)
         delay(run, 100)
       }
     }
--- src/n.js
+++ src/n.js
@@ -1,3 +1,108 @@
 // @flow strict
 import * as S from './stream'
+import * as D from './disposable'
 import * as M from './m'
+
+export class Patch {
+  patch: Node => void
+  constructor(patch: $PropertyType<Patch, 'patch'>) {
+    this.patch = patch
+  }
+}
+
+type SS<A> = S.S<A> | A
+
+type N$Pith = ((N | S.S<Patch>) => void) => void
+
+export class N {
+  constructor(create: () => Node, eq: Node => ?Node, pith: SS<N$Pith>) {
+    this.create = create
+    this.eq = eq
+    this.patches =
+      pith instanceof S.S
+        ? S.switchLatest(pith.map(pith => bark(ring(pith))))
+        : bark(ring(pith))
+    function ring(pith) {
+      return o => {
+        pith(v => {
+          o(v)
+        })
+      }
+    }
+  }
+  create: () => Node
+  eq: Node => ?Node
+  patches: S.S<Patch>
+}
+
+function bark(pith: N$Pith): S.S<Patch> {
+  return M.bark(o => {
+    const pnodes: Array<N> = []
+    o(
+      S.at(
+        patch(parent => {
+          const pnodesLength = pnodes.length
+          const childNodes = parent.childNodes
+          for (var index = 0; index < pnodesLength; index++) {
+            const pi = pnodes[index]
+            var li: ?Node
+            for (var i = index, l = childNodes.length; i < l; i++)
+              if ((li = pi.eq(parent.childNodes[i]))) break
+            if (li == null) parent.insertBefore(pi.create(), childNodes[index])
+            else if (i !== index) parent.insertBefore(li, childNodes[index])
+          }
+          for (var i = childNodes.length - 1; i >= pnodesLength; i--)
+            parent.removeChild(childNodes[i])
+        })
+      )
+    )
+    pith(v => {
+      if (v instanceof S.S) o(v)
+      else {
+        const index = pnodes.length
+
+        pnodes.push(v)
+        o(
+          v.patches.map(p => patch(parent => p.patch(parent.childNodes[index])))
+        )
+      }
+    })
+  })
+}
+
+export const patch = (patch: $PropertyType<Patch, 'patch'>): Patch =>
+  new Patch(patch)
+
+export const node = (
+  create: $PropertyType<N, 'create'>,
+  eq: $PropertyType<N, 'eq'>,
+  pith: SS<N$Pith>
+): N => new N(create, eq, pith)
+
+export const elm = (tag: string, pith: SS<N$Pith>, key?: ?string): N => {
+  const TAG = tag.toUpperCase()
+  return node(
+    () => document.createElement(tag),
+    n =>
+      n instanceof HTMLElement &&
+      n.nodeName === TAG &&
+      (key == null || n.dataset.key === key)
+        ? n
+        : null,
+    pith
+  )
+}
+
+export const text = (texts: SS<string>): N =>
+  node(
+    () => document.createTextNode(''),
+    n => (n instanceof Text ? n : null),
+    o =>
+      o(
+        (texts instanceof S.S ? texts : S.at(texts)).map(text =>
+          patch(n => {
+            n.textContent = text
+          })
+        )
+      )
+  )
