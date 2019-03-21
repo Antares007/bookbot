ece2282fa713da48f8b240a21bfd28e5f3defc4d
Archil Bolkvadze <a.bolkvadze@gmail.com>
2019-03-22 00:27:14 +0400

    delete trush

--- src/trush/io.js
+++
@@ -1,6 +0,0 @@
-// @flow
-export type IO<A, O, B> = ((O) => void) => A => B
-
-export function omap<A, O, B, R>(f: O => R): (IO<A, O, B>) => IO<A, R, B> {
-  return io => o => i => io(a => o(f(a)))(i)
-}
--- src/trush/list.js
+++
@@ -1,6 +0,0 @@
-// @flow strict
-export type List<A> = null | [A, List<A>]
-
-export function cons<A>(head: A, tail: List<A>): List<A> {
-  return [head, tail]
-}
