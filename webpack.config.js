eab8820d2f14dc2bcc2aeeb29db66a82182d7f54
Archil Bolkvadze <a.bolkvadze@gmail.com>
2019-03-23 00:45:51 +0400

    bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,12 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
-  entry: './src/show.js',
+  entry: './src/counter3.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   },
   module: {
-    rules: [
-      { test: /\.js$/, use: 'babel-loader' }
-    ]
+    rules: [{ test: /\.js$/, use: 'babel-loader' }]
   }
-};
+}
