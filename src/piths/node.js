49ae205e682ea73b73f92b1f758ac92dd777ff8a
Archil Bolkvadze <a.bolkvadze@gmail.com>
2019-04-15 22:20:59 +0400

    sync

--- src/piths/node.js
+++ src/piths/node.js
@@ -1,37 +1,66 @@
-// @flow strict
+// @flow
 import * as S from '../S'
 
 type SS<A> = S.S<A> | A
 
-declare export class NodePith {
-  pith: (
-    (SS<NodePith | TextPith>) => void,
-    (SS<(Node) => void>) => void
-  ) => void;
+export class NPith<T: Node> {
+  pith: ({ o: (SS<Nodes>) => void, patch: (SS<(T) => void>) => void }) => void
+  constructor(pith: $PropertyType<NPith<T>, 'pith'>) {
+    this.pith = pith
+  }
 }
-declare export function node(pith: $PropertyType<NodePith, 'pith'>): NodePith
 
-declare export class TextPith {
-  pith: ((SS<string>) => void) => void;
+type Nodes = Div | Button | string
+
+export class Div extends NPith<HTMLDivElement> {}
+export class Button extends NPith<HTMLButtonElement> {}
+
+export const div = (pith: $PropertyType<Div, 'pith'>): Div => new Div(pith)
+
+export const button = (pith: $PropertyType<Button, 'pith'>): Button =>
+  new Button(pith)
+
+const ring = <N: Node>(
+  p: (
+    { o: (SS<Nodes>) => void, patch: (SS<(N => void) | boolean>) => void },
+    number
+  ) => void
+): $PropertyType<NPith<N>, 'pith'> => {
+  return o => {}
 }
-declare export function text(pith: $PropertyType<TextPith, 'pith'>): TextPith
 
-node((o, op) => {
-  op(S.d(n => {}))
-  o(
-    node(o => {
-      o(
-        text(o => {
-          o(S.d('a'))
-          o('b')
+const see = div(
+  ring(({ o, patch }, i) => {
+    patch(a => {})
+    patch(true)
+    o(
+      button(
+        ring(({ o, patch }, i) => {
+          patch(n => {})
         })
       )
-    })
-  )
-  o(
-    text(o => {
-      o(S.d('a'))
-      o('b')
-    })
-  )
-})
+    )
+    o(div(({ o, patch }, i) => {}))
+  })
+)
+
+const counter = (d: number) =>
+  div(({ o }) => {
+    o(
+      button(({ o }) => {
+        o('+')
+        d > 0 && o(counter(d - 1))
+      })
+    )
+    o(
+      button(({ o }) => {
+        o('-')
+        d > 0 && o(counter(d - 1))
+      })
+    )
+    o('0')
+  })
+
+function run<N: Node>(pith: SS<NPith<N>>) {}
+
+run(counter(3))
