c00b8581530139757db0cf59810effec2963d84d
Archil Bolkvadze <a.bolkvadze@gmail.com>
2019-04-27 17:32:29 +0400

    add todo sample

--- src/todo.js
+++ src/todo.js
@@ -1,114 +1,94 @@
-// flow
+// @flow strict
 import * as S from './S'
 import type { NPith } from './N'
 import * as N from './N'
 
 const initState = {
-  inputText: '',
-  todos: []
+  inputText: 'b',
+  todos: [{ name: 'a', completed: false }]
 }
 
-type Model = typeof initState
+type Model = {
+  inputText: string,
+  todos: Array<{ name: string, completed: boolean }>
+}
 
-const todoApp = N.elm<Model>('div', (o, i) => {
-  o(
+const todo = N.elm<Model>('div', (o, i) => {
+  o.node(
     N.elm('div', (o, i) => {
-      o(
+      o.node(
         N.elm('input', (o, i) => {
-          o(
-            i.states.map(m =>
-              N.patch(n => {
-                if (n instanceof HTMLInputElement && n.value !== m.inputText)
-                  n.value = m.inputText
-              })
-            )
+          const on = new S.On(i.ref)
+          o.patch(
+            i.states.map(m => n => {
+              if (n instanceof HTMLInputElement && n.value !== m.inputText) n.value = m.inputText
+            })
           )
-          o(
-            i.on.input().map(e =>
-              N.r(s => ({
-                ...s,
-                inputText:
-                  e.target instanceof HTMLInputElement ? e.target.value : ''
-              }))
-            )
+          o.reduce(
+            on.input().map(e => s => ({
+              ...s,
+              inputText: e.target instanceof HTMLInputElement ? e.target.value : ''
+            }))
           )
         })
       )
-      o(
+      o.node(
         N.elm('button', (o, i) => {
-          o(N.text('add'))
-          o(
-            i.on.click().map(_ =>
-              N.r(s => ({
-                inputText: '',
-                todos: s.todos.concat([{ name: s.inputText, completed: false }])
-              }))
-            )
+          const on = new S.On(i.ref)
+          o.node(N.text('add'))
+          o.patch(
+            i.states.map(m => n => {
+              if (n instanceof HTMLElement)
+                if (m.inputText.trim().length > 0) n.removeAttribute('disabled')
+                else n.setAttribute('disabled', '')
+            })
           )
-          o(
-            i.states.map(m =>
-              N.patch(n => {
-                if (n instanceof HTMLElement)
-                  if (m.inputText.trim().length > 0)
-                    n.removeAttribute('disabled')
-                  else n.setAttribute('disabled', '')
-              })
-            )
+          o.reduce(
+            on.click().map(_ => s => ({
+              inputText: '',
+              todos: s.todos.concat([{ name: s.inputText, completed: false }])
+            }))
           )
         })
       )
     })
   )
-  o(
-    N.elm(
-      'ul',
-      i.states
-        .map(m => m.todos)
-        .skipEquals()
-        .map(todos => (o, i) => {
+  o.node(
+    i.states
+      .map(m => m.todos)
+      .skipEquals()
+      .map(todos =>
+        N.elm('ul', (o, i) => {
           for (let todo of todos)
-            o(
-              N.elm(
-                'li',
-                o => {
-                  o(
-                    N.elm('div', o => {
-                      o(N.text(todo.name))
-                      o(
-                        N.elm('button', (o, i) => {
-                          o(N.text('-'))
-                          o(
-                            i.on.click().map(_ =>
-                              N.r(s => ({
-                                ...s,
-                                todos: s.todos.filter(x => x !== todo)
-                              }))
-                            )
-                          )
-                        })
-                      )
-                    })
-                  )
-                },
-                todo.name
-              )
+            o.node(
+              N.elm('li', (o, i) => {
+                o.node(N.text(todo.name))
+                o.node(
+                  N.elm('button', (o, i) => {
+                    const on = new S.On(i.ref)
+                    o.node(N.text('remove'))
+                    o.reduce(
+                      on.click().map(_ => s => ({
+                        ...s,
+                        todos: s.todos.filter(x => x !== todo)
+                      }))
+                    )
+                  })
+                )
+              })
             )
         })
-    )
+      )
   )
 })
 
 const rootNode = document.getElementById('root-node')
 if (!rootNode) throw new Error('cant find root-node')
 
-N.run(
-  rootNode,
-  JSON.parse(localStorage.getItem('todo') || JSON.stringify(initState)),
-  todoApp
-).run(e => {
-  if (e instanceof S.Next) {
-    const ns = JSON.stringify(e.value, null, '  ')
-    localStorage.setItem('todo', ns)
-    console.log(ns)
-  } else console.info(e)
+const states = N.runO(rootNode, initState, todo)
+
+states.run(e => {
+  if (e instanceof S.Next) console.log(JSON.stringify(e.value, null, '  '))
+  else if (e instanceof Error) console.error(e)
+  else console.info(e)
 })
