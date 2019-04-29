9f7627e2a14bfb31ad279bc0ecf2b4503b324a33
Archil Bolkvadze <a.bolkvadze@gmail.com>
2019-04-29 14:10:02 +0400

    refactor counter buttons

--- src/counter3.js
+++ src/counter3.js
@@ -4,7 +4,17 @@ import * as N from './N'
 
 const pi2 = Math.PI * 2
 const r = 10
-const counter = (d: number): N.N<{ n: number }> =>
+
+const button = (label, action, styles, depth) =>
+  N.button((o, i) => {
+    o(label)
+    o.style({ position: 'relative', outline: 'none' })
+    o.style(styles)
+    o.reduce(i.on.click().map(_ => s => ({ ...s, n: s.n + action })))
+    depth > 0 && o(N.extend('+', { n: 0 })(counter(depth - 1)))
+  })
+
+const counter = (depth: number): N.N<{ n: number }> =>
   N.div((o, i) => {
     o.style({ padding: '5px 10px', 'text-align': 'center' })
     const colors = S.periodic(20)
@@ -12,50 +22,38 @@ const counter = (d: number): N.N<{ n: number }> =>
       .map(i => {
         const s = Math.sin(i)
         const c = Math.cos(i)
-        const c1 = 100 + d * 20 + Math.floor(30 * s)
-        const c2 = 100 + d * 20 + Math.floor(30 * c)
+        const c1 = 100 + depth * 20 + Math.floor(30 * s)
+        const c2 = 100 + depth * 20 + Math.floor(30 * c)
         return { s, c, c1, c2 }
       })
 
     o(
-      N.button((o, i) => {
-        o('+')
-        o.style({
-          position: 'relative',
-          outline: 'none'
-        })
-        o.style(
-          S.map(({ s, c, c1, c2 }) => {
-            return {
-              'border-radius': Math.abs(Math.floor(s * 20)) + 'px',
-              left: Math.floor(r * c) + 'px',
-              top: Math.floor(r * s) + 'px',
-              'background-color': `rgb(255, ${c1}, ${c2})`
-            }
-          }, colors)
-        )
-        o.reduce(i.on.click().map(_ => s => ({ ...s, n: s.n + 1 })))
-        d > 0 && o(N.extend('+', { n: 0 })(counter(d - 1)))
-      }),
-      N.button((o, i) => {
-        o('-')
-        o.style({
-          position: 'relative',
-          outline: 'none'
-        })
-        o.style(
-          S.map(({ s, c, c1, c2 }) => {
-            return {
-              'border-radius': Math.abs(Math.floor(c * 20)) + 'px',
-              left: Math.floor(r * s) + 'px',
-              top: Math.floor(r * c) + 'px',
-              'background-color': `rgb(${c1}, ${c2}, 255)`
-            }
-          }, colors)
-        )
-        o.reduce(i.on.click().map(_ => s => ({ ...s, n: s.n - 1 })))
-        d > 0 && o(N.extend('-', { n: 0 })(counter(d - 1)))
-      }),
+      button(
+        '+',
+        1,
+        S.map(({ s, c, c1, c2 }) => {
+          return {
+            'border-radius': Math.abs(Math.floor(s * 20)) + 'px',
+            //left: Math.floor(r * c) + 'px',
+            //top: Math.floor(r * s) + 'px',
+            'background-color': `rgb(255, ${c1}, ${c2})`
+          }
+        }, colors),
+        depth
+      ),
+      button(
+        '-',
+        -1,
+        S.map(({ s, c, c1, c2 }) => {
+          return {
+            'border-radius': Math.abs(Math.floor(c * 20)) + 'px',
+            //left: Math.floor(r * s) + 'px',
+            //top: Math.floor(r * c) + 'px',
+            'background-color': `rgb(${c1}, ${c2}, 255)`
+          }
+        }, colors),
+        depth
+      ),
       i.states.map(s => s.n + '')
     )
   })
