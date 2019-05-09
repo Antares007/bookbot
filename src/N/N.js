58eab7236c00774432d686978c9abbf903566ffb
Archil Bolkvadze <a.bolkvadze@gmail.com>
2019-05-09 09:40:57 +0400

    sync

--- src/N/N.js
+++ src/N/N.js
@@ -32,23 +32,29 @@ export const elmNS = (ns: string, tag: string, pith: NPith): N => ({
 export const text = (value: string): N => ({ type: 'text', tag: '#text', value })
 export const comment = (value: string): N => ({ type: 'comment', tag: '#comment', value })
 
-export function run(n: N): S.S<(Node) => void> {
-  switch (n.type) {
-    case 'text':
-    case 'comment':
-      return S.d(parent => {
-        if (parent.textContent !== n.value) parent.textContent = n.value
-      })
-    case 'element':
-    case 'elementNS':
-      return S.s(runPith(n.pith))
-    default:
-      throw new Error('never')
+const cache: Map<N, S.S<(Node) => void>> = new Map()
+
+function memoized<A, B>(f: A => B): A => B {
+  const cache = new Map()
+  return a => {
+    var b = cache.get(a)
+    if (b) return b
+    b = f(a)
+    cache.set(a, b)
+    return b
   }
 }
 
-function runPith(pith: NPith) {
-  return o => {
+const run: N => S.S<(Node) => void> = memoized(n =>
+  n.type === 'text' || n.type === 'comment'
+    ? S.d(parent => {
+        if (parent.textContent !== n.value) parent.textContent = n.value
+      })
+    : runPith(n.pith)
+)
+
+function runPith(pith: NPith): S.S<(Node) => void> {
+  return S.s(o => {
     const dmap = new Map()
     o(D.create(() => dmap.forEach(d => d.dispose())))
 
@@ -58,7 +64,7 @@ function runPith(pith: NPith) {
     const mergeO = makeMergeO(o)
 
     var i = 0
-    const ns: Array<[number, N]> = []
+    const ns: Array<{ i: number, n: N, d: D.Disposable }> = []
     pith(
       Object.assign(
         ss => {
@@ -66,10 +72,19 @@ function runPith(pith: NPith) {
           const map = (n: N) => {
             const at = index
             const ap = findAppendPosition(at, ns)
-            var li = ns[ap]
-            if (ap === -1 || li[0] !== at) {
-              li = [at, n]
-              ns.splice(ap + 1, 0, li)
+            if (ap === -1 || ns[ap][0] !== at) {
+              ns.splice(ap + 1, 0, {
+                i: at,
+                n,
+                d: mergeO(run(n).map(patch => node => patch(node.childNodes[ap + 1])))
+              })
+              for (var k = ap + 2, l = ns.length; k < l; k++);
+              return (node: Node) => {
+                //
+              }
+            } else {
+              ns[ap].n = n
+              ns[ap].d = mergeO(run(n))
             }
             return (node: Node) => {}
           }
@@ -83,40 +98,6 @@ function runPith(pith: NPith) {
       ),
       { ref }
     )
-
-    //    const ssnodes: Array<SS<N>> = []
-    //    const patchess = []
-    //    const patches = []
-    //    var i = 0
-    //    pith(
-    //      Object.assign(
-    //        v => {
-    //          const index = i++
-    //          ssnodes.push(v)
-    //        },
-    //        {
-    //          patch: v => {
-    //            if (v instanceof S.S) patchess.push(v)
-    //            else patches.push(v)
-    //          }
-    //        }
-    //      ),
-    //      { ref }
-    //    )
-    //
-    //    var childPatches: Array<S.S<(Node) => void>>
-    //    var childNodes: Array<N>
-    //    start(
-    //      combineSS(
-    //        nodes => {
-    //          childNodes = nodes
-    //          childPatches = new Array(nodes.length)
-    //
-    //          for (var i = 0, l = nodes.length; i < l; i++)
-    //            start((childPatches[i] = runOn(nodes[i], i)))
-    //
-    //          for (var i = 0, l = patchess.length; i < l; i++) start(patchess[i])
-    //
     //          return parent => {
     //            const pnodesLength = nodes.length
     //            const childNodes = parent.childNodes
@@ -134,25 +115,34 @@ function runPith(pith: NPith) {
     //            for (var i = 0, l = patches.length; i < l; i++) patches[i](parent)
     //            refO(parent)
     //          }
-    //        },
-    //        (node, index) => {
-    //          const oldPatch = childPatches[index]
-    //          start((childPatches[index] = runOn(node, index)))
-    //          stop(oldPatch)
-    //          const oldNode = childNodes[index]
-    //          if (oldNode.tag !== node.tag || (node.key && oldNode.key !== node.key)) {
-    //            childNodes[index] = node
-    //            return parent => {
-    //              const on = parent.childNodes[index]
-    //              if (eq(on, node)) return
-    //              parent.insertBefore(create(node), on)
-    //              console.log('rm_', parent.removeChild(on))
-    //            }
-    //          } else return null
-    //        },
-    //        ssnodes
-    //      ).filterJust(x => x)
-    //    )
+  })
+}
+
+function makeMergeO<A>(
+  o: (S.Next<A> | S.End | Error | D.Disposable) => void
+): (S.S<A> | A) => D.Disposable {
+  const dmap = new Map()
+  o(D.create(() => dmap.forEach(d => d.dispose())))
+  return x => {
+    var d =
+      x instanceof S.S
+        ? x.run(e => {
+            if (e instanceof S.End) {
+              dmap.delete(x)
+              if (dmap.size === 0) o(e)
+            } else o(e)
+          })
+        : S.delay(() => {
+            dmap.delete(x)
+            if (dmap.size === 0) o((d = S.delay(() => o(S.end))))
+            o(S.next(x))
+          })
+    const ret = D.create(() => {
+      dmap.delete(x)
+      d.dispose()
+    })
+    dmap.set(x, ret)
+    return ret
   }
 }
 
@@ -228,13 +218,16 @@ function combineSS<A, B>(
   })
 }
 
-export function findAppendPosition<T>(n: number, line: Array<[number, T]>): number {
+export function findAppendPosition<T>(
+  n: number,
+  line: Array<{ i: number, n: N, d: D.Disposable }>
+): number {
   var l = 0
   var r = line.length
   while (true) {
     if (l < r) {
       const m = ~~((l + r) / 2) | 0
-      if (line[m][0] > n) {
+      if (line[m].i > n) {
         r = m
         continue
       } else {
@@ -246,32 +239,4 @@ export function findAppendPosition<T>(n: number, line: Array<[number, T]>): numb
     }
   }
   throw new Error('never')
-}
-
-function makeMergeO<A>(
-  o: (S.Next<A> | S.End | Error | D.Disposable) => void
-): (S.S<A> | A) => D.Disposable {
-  const dmap = new Map()
-  o(D.create(() => dmap.forEach(d => d.dispose())))
-  return x => {
-    var d =
-      x instanceof S.S
-        ? x.run(e => {
-            if (e instanceof S.End) {
-              dmap.delete(x)
-              if (dmap.size === 0) o(e)
-            } else o(e)
-          })
-        : S.delay(() => {
-            dmap.delete(x)
-            if (dmap.size === 0) o((d = S.delay(() => o(S.end))))
-            o(S.next(x))
-          })
-    const ret = D.create(() => {
-      dmap.delete(x)
-      d.dispose()
-    })
-    dmap.set(x, ret)
-    return ret
-  }
 }
