9fa205d381cbba6aa5f6842cba42cc729cb451ab
Archil Bolkvadze <a.bolkvadze@gmail.com>
2019-06-02 16:23:22 +0400

    delete old staff

--- src/N/streamstaff.js
+++
@@ -1,9 +0,0 @@
-// @flow strict
-import * as S from '../S'
-import * as D from '../S/Disposable'
-
-export type SS<A> = S.S<A> | A
-
-export function ssmap<A, B>(f: A => B, ss: SS<A>): SS<B> {
-  return ss instanceof S.S ? ss.map(f) : f(ss)
-}
