4c4a7e680d739d350d4547d78300efcd442d07b5
Archil Bolkvadze <a.bolkvadze@gmail.com>
2019-06-04 11:51:24 +0400

    sync

--- src/tP.js
+++ src/tP.js
@@ -1,6 +1,7 @@
 // @flow strict
 import * as Schdlr from './S/scheduler'
 import * as D from './S/Disposable'
+import * as S from './tS'
 
 export opaque type PPith<+A> = (
   ({ R: 'next', +value: A } | { R: 'error', error: Error }) => void
@@ -9,3 +10,14 @@ export opaque type PPith<+A> = (
 export function p<+A>(
   pith: (({ R: 'next', +value: A } | { R: 'error', error: Error }) => void) => void
 ) {}
+
+function spmap<A, B>(f: A => B, sp: S.SPith<Promise<A>>): S.SPith<Promise<B>> {
+  return S.map(p => p.then(f), sp)
+}
+
+function spcombine<A, B>(
+  f: (...Array<A>) => B,
+  ...sps: Array<S.SPith<Promise<A>>>
+): S.SPith<Promise<B>> {
+  return S.combine((...ps) => Promise.all(ps).then(x => f(...x)), ...sps)
+}
