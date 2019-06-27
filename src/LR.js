b5b700beb7918fd6d126758bcede02fd6e9751dc
Archil Bolkvadze <a.bolkvadze@gmail.com>
2019-06-27 15:06:43 +0400

    simplify LR

--- src/LR.js
+++ src/LR.js
@@ -1,5 +1,13 @@
-export const left = value => ({ T: 'left', value })
-export const right = value => ({ T: 'right', value })
-export const mappend = (l, r) => (l.T === 'right' && r.T === 'right' ? r : l)
-export const map = (f, lr) => (lr.T === 'right' ? right(f(lr.value)) : lr)
-export const flatMap = (f, lr) => (lr.T === 'right' ? f(lr.value) : lr)
+// @flow strict
+
+export type Right<+T> = {| +T: 'right', +value: T |}
+export type Left<+T> = {| +T: 'left', +value: T |}
+
+export const left: <L>(L) => Left<L> = <L>(value: L) => ({ T: 'left', value })
+export const right: <R>(R) => Right<R> = <R>(value: R) => ({ T: 'right', value })
+export const map = <A, B, L>(f: A => B, lr: Right<A> | Left<L>): Right<B> | Left<L> =>
+  lr.T === 'right' ? right(f(lr.value)) : lr
+export const flatMap = <A, B, L>(
+  f: A => Right<B> | Left<L>,
+  lr: Right<A> | Left<L>
+): Right<B> | Left<L> => (lr.T === 'right' ? f(lr.value) : lr)
