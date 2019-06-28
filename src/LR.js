931e9d7ed046964c869dfeb96afae76ea9973a3f
Archil Bolkvadze <a.bolkvadze@gmail.com>
2019-06-28 09:39:07 +0400

    clean lr

--- src/LR.js
+++ src/LR.js
@@ -3,10 +3,13 @@
 export type Right<+T> = {| +T: 'right', +value: T |}
 export type Left<+T> = {| +T: 'left', +value: T |}
 
-export const left: <L>(L) => Left<L> = <L>(value: L) => ({ T: 'left', value })
-export const right: <R>(R) => Right<R> = <R>(value: R) => ({ T: 'right', value })
+export const left = <L>(value: L): Left<L> => ({ T: 'left', value })
+
+export const right = <R>(value: R): Right<R> => ({ T: 'right', value })
+
 export const map = <A, B, L>(f: A => B, lr: Right<A> | Left<L>): Right<B> | Left<L> =>
   lr.T === 'right' ? right(f(lr.value)) : lr
+
 export const flatMap = <A, B, L>(
   f: A => Right<B> | Left<L>,
   lr: Right<A> | Left<L>
