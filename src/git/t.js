0169ca1fff28479abb00f1bc039890fad4abd08b
Archil Bolkvadze <a.bolkvadze@gmail.com>
2019-06-28 19:10:10 +0400

    sync

--- src/git/t.js
+++ src/git/t.js
@@ -1,6 +1,6 @@
 // @flow strict
 import * as OC from './object-codec'
-import * as CB from '../CB'
+import * as S from '../S'
 import * as M from '../M'
 import * as LR from '../LR'
 import * as fs from 'fs'
@@ -9,29 +9,45 @@ import { join as pathJoin } from 'path'
 
 export type Hash = string
 
-export const mkrepo: string => { load: Hash => CB.CBPith<OC.Tree | OC.Blob | OC.Commit> } = M.ab(
-  rootPath => {
-    return {
-      load: M.ab(hash =>
-        CB.p(o => {
-          fs.readFile(hashToPath(hash), (err, buffer) => {
-            if (err) {
-              if (err.code !== 'ENOENT') o(LR.left(err))
-              else {
-              }
-            } else {
-              zlib.inflate(buffer, (err, buffer) => {
-                if (err) o(LR.left(err))
-                else o(LR.right(OC.deframe(buffer)))
-              })
-            }
-          })
-        })
-      )
-    }
-
-    function hashToPath(hash) {
-      return pathJoin(rootPath, 'objects', hash.substring(0, 2), hash.substring(2))
-    }
-  }
+const deframe = S.map(OC.deframe)
+const inflate = S.flatMap(buffer =>
+  S.fromCB(cb => zlib.inflate(buffer, (err: ?Error, buffer: Buffer) => cb(err, buffer)))
 )
+
+export const loadHash = (rootPath: string) => (hash: string) =>
+  deframe(
+    inflate(
+      S.tryCatch(
+        S.fromCB(cb =>
+          fs.readFile(pathJoin(rootPath, 'objects', hash.substring(0, 2), hash.substring(2)), cb)
+        )
+      )
+    )
+  )
+
+//export const mkrepo: string => { load: Hash => CB.CBPith<OC.Tree | OC.Blob | OC.Commit> } = M.ab(
+//  rootPath => {
+//    return {
+//      load: M.ab(hash =>
+//        CB.p(o => {
+//          fs.readFile(hashToPath(hash), (err, buffer) => {
+//            if (err) {
+//              if (err.code !== 'ENOENT') o(LR.left(err))
+//              else {
+//              }
+//            } else {
+//              zlib.inflate(buffer, (err, buffer) => {
+//                if (err) o(LR.left(err))
+//                else o(LR.right(OC.deframe(buffer)))
+//              })
+//            }
+//          })
+//        })
+//      )
+//    }
+//
+//    function hashToPath(hash) {
+//      return pathJoin(rootPath, 'objects', hash.substring(0, 2), hash.substring(2))
+//    }
+//  }
+//)
