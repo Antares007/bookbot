adbc9caa76145b745effffa4f718797ab8551863
Archil Bolkvadze <a.bolkvadze@gmail.com>
2020-07-29 19:13:23 +0400

    sync

--- src/counter5.js
+++ src/counter5.js
@@ -1,68 +1,51 @@
-// @flow strict
-const elm = (tag, pith) => ({ type: ("elm": "elm"), tag, pith });
+// @flow
+const draw = (ring) =>
+  typeof ring === "function" ? { r: ("draw": "draw"), ring } : ring.ring;
+const elm = (tag, pith) => ({ r: ("elm": "elm"), tag, pith });
 const button = (pith) => elm("BUTTON", pith);
 const div = (pith) => elm("DIV", pith);
 const onClick = (f: (MouseEvent) => mixed) => ({
-  type: ("handler": "handler"),
+  r: ("handler": "handler"),
   f,
 });
-const ring = (props, f) => ({ type: ("ring": "ring"), props, f });
+const reduce = (g) => ({ r: ("reduce": "reduce"), g });
 var di = 0;
-function counter(o, props: {| d: number |}) {
-  const ob = o;
+function counter(o, d: number = 3) {
   o(
-    button(
-      ring(props, (o, { d }) => {
-        o("+");
-        o(
-          onClick((e) => {
-            ob((s) => {
-              return { ...s, n: s.n + 1 };
-            });
-            ob(
-              ring({ d }, (o, p) => {
-                counter((r) => {
-                  if (typeof r === "function")
-                    o((s) => {
-                      return { ...s, ["+"]: r(s["+"] || { n: 1 }) };
-                    });
-                  else o(r);
-                }, p);
-              })
-            );
-          })
-        );
-        if (d > 0) o(div(ring({ d: d - 1 }, counter)));
-      })
-    )
+    button((o) => {
+      o("+");
+      if (d > 0) o(div((o) => counter((r) => o(r), d - 1)));
+    })
   );
   o(
-    button(
-      ring(props, (o, { d }) => {
-        o("-");
-        if (d > 0) o(div(ring({ d: d - 1 }, counter)));
-      })
-    )
+    button((o) => {
+      o("-");
+      if (d > 0) o(div((o) => counter(o, d - 1)));
+    })
   );
-  o((s) => {
-    o(s.n + "");
-    return s;
-  });
+  o(
+    reduce((s) => {
+      o(s.n + "");
+      return s;
+    })
+  );
 }
 
-// function page1(o, p) {
-//   o("page 1");
-//   o(div({ d: 1 }, switcher));
-// }
-// function page2(o, p) {}
-// function switcher(o, p) {}
-
-const mkpith = (o, elm: HTMLElement) => {
+const mkpith = (
+  o,
+  elm: HTMLElement,
+  vnode = { disposables: [], childs: [] }
+) => {
   var lastIndex;
   const { childNodes } = elm;
   return function pith(x) {
     if (typeof x === "function") {
-      o(x);
+      // ring
+      lastIndex = 0;
+      x(pith);
+      for (let l = childNodes.length; l > lastIndex; l--)
+        elm.removeChild(childNodes[lastIndex]);
+      return;
     } else if (typeof x === "string") {
       const index = lastIndex++;
       for (let i = index, l = childNodes.length; i < l; i++) {
@@ -73,26 +56,21 @@ const mkpith = (o, elm: HTMLElement) => {
         }
       }
       elm.insertBefore(document.createTextNode(x), childNodes[index]);
-    } else if (x == null) {
-    } else if (x.type === "ring") {
-      lastIndex = 0;
-      x.f(pith, x.props);
-      for (let l = childNodes.length; l > lastIndex; l--)
-        elm.removeChild(childNodes[lastIndex]);
-      return;
-    } else if (x.type === "elm") {
+    } else if (x.r === "reduce") {
+      o(x.g);
+    } else if (x.r === "elm") {
       const index = lastIndex++;
       for (let i = index, l = childNodes.length; i < l; i++) {
         let n = childNodes[i];
         if (n instanceof HTMLElement && n.nodeName === x.tag) {
           if (index < i) elm.insertBefore(n, childNodes[index]);
-          return mkpith(o, n)(ring(x.pith.props, x.pith.f));
+          return mkpith(o, n)(x.pith);
         }
       }
       mkpith(
         o,
         elm.insertBefore(document.createElement(x.tag), childNodes[index])
-      )(ring(x.pith.props, x.pith.f));
+      )(x.pith);
     } else {
       elm.addEventListener("click", x.f);
     }
@@ -100,11 +78,21 @@ const mkpith = (o, elm: HTMLElement) => {
 };
 const rootNode = document.getElementById("root-node");
 if (!rootNode) throw new Error("cant find root-node");
+
 var state: { n: number } = { n: 0 };
 console.info(state);
+
 const bark = mkpith((r) => {
+  const os = state;
   state = r(state);
-  console.info(state);
+  if (os !== state) console.info(state);
 }, rootNode);
-bark(ring({ d: 1 }, counter));
-bark();
+
+bark(counter);
+
+// function page1(o, p) {
+//   o("page 1");
+//   o(div({ d: 1 }, switcher));
+// }
+// function page2(o, p) {}
+// function switcher(o, p) {}
