1bd9dfd9621f9ca1f153a2570976c636b3d53cd2
Archil Bolkvadze <a.bolkvadze@gmail.com>
2020-07-29 21:18:51 +0400

    rename

--- src/counter5.js
+++ src/counter5.js
@@ -1,31 +1,31 @@
 // @flow
-const draw = (ring) =>
-  typeof ring === "function" ? { r: ("draw": "draw"), ring } : ring.ring;
-const elm = (tag, pith) => ({ r: ("elm": "elm"), tag, pith });
-const button = (pith) => elm("BUTTON", pith);
-const div = (pith) => elm("DIV", pith);
+const fun = (v) => ({ _: ("function": "function"), v });
+const str = (v) => ({ _: ("string": "string"), v });
+const elm = (tag, pith) => ({ _: ("elm": "elm"), tag, pith: fun(pith) });
 const onClick = (f: (MouseEvent) => mixed) => ({
-  r: ("handler": "handler"),
+  _: ("handler": "handler"),
   f,
 });
-const reduce = (g) => ({ r: ("reduce": "reduce"), g });
+const reduce = (g) => ({ _: ("reduce": "reduce"), g });
+const button = (pith) => elm("BUTTON", pith);
+const div = (pith) => elm("DIV", pith);
 var di = 0;
-function counter(o, d: number = 3) {
+function counter(o, d: number = 1) {
   o(
     button((o) => {
-      o("+");
+      o(str("+"));
       if (d > 0) o(div((o) => counter((r) => o(r), d - 1)));
     })
   );
   o(
     button((o) => {
-      o("-");
+      o(str("-"));
       if (d > 0) o(div((o) => counter(o, d - 1)));
     })
   );
   o(
     reduce((s) => {
-      o(s.n + "");
+      o(str(s.n + ""));
       return s;
     })
   );
@@ -39,14 +39,13 @@ const mkpith = (
   var lastIndex;
   const { childNodes } = elm;
   return function pith(x) {
-    if (typeof x === "function") {
-      // ring
+    if (x._ === "function") {
       lastIndex = 0;
-      x(pith);
+      x.v(pith);
       for (let l = childNodes.length; l > lastIndex; l--)
         elm.removeChild(childNodes[lastIndex]);
       return;
-    } else if (typeof x === "string") {
+    } else if (x._ === "string") {
       const index = lastIndex++;
       for (let i = index, l = childNodes.length; i < l; i++) {
         let n = childNodes[i];
@@ -55,10 +54,10 @@ const mkpith = (
           return;
         }
       }
-      elm.insertBefore(document.createTextNode(x), childNodes[index]);
-    } else if (x.r === "reduce") {
+      elm.insertBefore(document.createTextNode(x.v), childNodes[index]);
+    } else if (x._ === "reduce") {
       o(x.g);
-    } else if (x.r === "elm") {
+    } else if (x._ === "elm") {
       const index = lastIndex++;
       for (let i = index, l = childNodes.length; i < l; i++) {
         let n = childNodes[i];
@@ -72,7 +71,7 @@ const mkpith = (
         elm.insertBefore(document.createElement(x.tag), childNodes[index])
       )(x.pith);
     } else {
-      elm.addEventListener("click", x.f);
+      //elm.addEventListener("click", x.f);
     }
   };
 };
@@ -88,7 +87,7 @@ const bark = mkpith((r) => {
   if (os !== state) console.info(state);
 }, rootNode);
 
-bark(counter);
+bark(fun(counter));
 
 // function page1(o, p) {
 //   o("page 1");
