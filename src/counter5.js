6dd3e55563c20512e888cfa0fdeb9cbbc10f2842
Archil Bolkvadze <a.bolkvadze@gmail.com>
2020-07-30 08:25:20 +0400

    extract pith

--- src/counter5.js
+++ src/counter5.js
@@ -1,4 +1,7 @@
-// @flow
+// @flow strict
+import { mkpith } from "./mkpith";
+import type { Pith } from "./mkpith";
+
 const elm = (tag, pith) => ({ _: ("elm": "elm"), tag, pith });
 const onClick = (f: (MouseEvent) => mixed) => ({
   _: ("handler": "handler"),
@@ -28,56 +31,7 @@ function counter(o: (Pith<{ n: number }>) => void, d: number = 1) {
     })
   );
 }
-type Ring<S> = ((Pith<S>) => void) => void;
-type Pith<S> =
-  | string
-  | Ring<S>
-  | {| _: "elm", tag: string, pith: Ring<S> |}
-  | {| _: "reduce", g: (S) => S |};
-const mkpith = <S>(
-  o: ((S) => S) => void,
-  elm: HTMLElement,
-  vnode = { disposables: [], childs: [] }
-): ((Pith<S>) => void) => {
-  var lastIndex;
-  const { childNodes } = elm;
-  return function pith(x): void {
-    if (typeof x === "function") {
-      lastIndex = 0;
-      x(pith);
-      for (let l = childNodes.length; l > lastIndex; l--)
-        elm.removeChild(childNodes[lastIndex]);
-      return;
-    } else if (typeof x === "string") {
-      const index = lastIndex++;
-      for (let i = index, l = childNodes.length; i < l; i++) {
-        let n = childNodes[i];
-        if (n.nodeType === 3 && n.textContent === x) {
-          if (index < i) elm.insertBefore(n, childNodes[index]);
-          return;
-        }
-      }
-      elm.insertBefore(document.createTextNode(x), childNodes[index]);
-    } else if (x._ === "reduce") {
-      o((s) => x.g(s));
-    } else if (x._ === "elm") {
-      const index = lastIndex++;
-      for (let i = index, l = childNodes.length; i < l; i++) {
-        let n = childNodes[i];
-        if (n instanceof HTMLElement && n.nodeName === x.tag) {
-          if (index < i) elm.insertBefore(n, childNodes[index]);
-          return mkpith(o, n)(x.pith);
-        }
-      }
-      mkpith(
-        o,
-        elm.insertBefore(document.createElement(x.tag), childNodes[index])
-      )(x.pith);
-    } else {
-      //elm.addEventListener("click", x.f);
-    }
-  };
-};
+
 const rootNode = document.getElementById("root-node");
 if (!rootNode) throw new Error("cant find root-node");
 
---
+++ src/mkpith.js
@@ -0,0 +1,52 @@
+// @flow strict
+export type Ring<S> = ((Pith<S>) => void) => void;
+export type Pith<S> =
+  | string
+  | Ring<S>
+  | {| _: "elm", tag: string, pith: Ring<S> |}
+  | {| _: "reduce", g: (S) => S |};
+
+export const mkpith = <S>(
+  o: ((S) => S) => void,
+  elm: HTMLElement,
+  vnode: {} = { disposables: [], childs: [] }
+): ((Pith<S>) => void) => {
+  var lastIndex;
+  const { childNodes } = elm;
+  return function pith(x): void {
+    if (typeof x === "function") {
+      lastIndex = 0;
+      x(pith);
+      for (let l = childNodes.length; l > lastIndex; l--)
+        elm.removeChild(childNodes[lastIndex]);
+      return;
+    } else if (typeof x === "string") {
+      const index = lastIndex++;
+      for (let i = index, l = childNodes.length; i < l; i++) {
+        let n = childNodes[i];
+        if (n.nodeType === 3 && n.textContent === x) {
+          if (index < i) elm.insertBefore(n, childNodes[index]);
+          return;
+        }
+      }
+      elm.insertBefore(document.createTextNode(x), childNodes[index]);
+    } else if (x._ === "reduce") {
+      o((s) => x.g(s));
+    } else if (x._ === "elm") {
+      const index = lastIndex++;
+      for (let i = index, l = childNodes.length; i < l; i++) {
+        let n = childNodes[i];
+        if (n instanceof HTMLElement && n.nodeName === x.tag) {
+          if (index < i) elm.insertBefore(n, childNodes[index]);
+          return mkpith(o, n)(x.pith);
+        }
+      }
+      mkpith(
+        o,
+        elm.insertBefore(document.createElement(x.tag), childNodes[index])
+      )(x.pith);
+    } else {
+      //elm.addEventListener("click", x.f);
+    }
+  };
+};
