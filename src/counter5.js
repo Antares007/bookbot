68839df16a9b1029042362bb4b89767099c53e5b
Archil Bolkvadze <a.bolkvadze@gmail.com>
2020-07-30 08:18:38 +0400

    add types

--- src/counter5.js
+++ src/counter5.js
@@ -1,7 +1,5 @@
 // @flow
-const fun = (v) => ({ _: ("function": "function"), v });
-const str = (v) => ({ _: ("string": "string"), v });
-const elm = (tag, pith) => ({ _: ("elm": "elm"), tag, pith: fun(pith) });
+const elm = (tag, pith) => ({ _: ("elm": "elm"), tag, pith });
 const onClick = (f: (MouseEvent) => mixed) => ({
   _: ("handler": "handler"),
   f,
@@ -10,42 +8,47 @@ const reduce = (g) => ({ _: ("reduce": "reduce"), g });
 const button = (pith) => elm("BUTTON", pith);
 const div = (pith) => elm("DIV", pith);
 var di = 0;
-function counter(o, d: number = 1) {
+function counter(o: (Pith<{ n: number }>) => void, d: number = 1) {
   o(
     button((o) => {
-      o(str("+"));
+      o("+");
       if (d > 0) o(div((o) => counter((r) => o(r), d - 1)));
     })
   );
   o(
     button((o) => {
-      o(str("-"));
+      o("-");
       if (d > 0) o(div((o) => counter(o, d - 1)));
     })
   );
   o(
     reduce((s) => {
-      o(str(s.n + ""));
+      o(s.n + "");
       return s;
     })
   );
 }
-
-const mkpith = (
-  o,
+type Ring<S> = ((Pith<S>) => void) => void;
+type Pith<S> =
+  | string
+  | Ring<S>
+  | {| _: "elm", tag: string, pith: Ring<S> |}
+  | {| _: "reduce", g: (S) => S |};
+const mkpith = <S>(
+  o: ((S) => S) => void,
   elm: HTMLElement,
   vnode = { disposables: [], childs: [] }
-) => {
+): ((Pith<S>) => void) => {
   var lastIndex;
   const { childNodes } = elm;
-  return function pith(x) {
-    if (x._ === "function") {
+  return function pith(x): void {
+    if (typeof x === "function") {
       lastIndex = 0;
-      x.v(pith);
+      x(pith);
       for (let l = childNodes.length; l > lastIndex; l--)
         elm.removeChild(childNodes[lastIndex]);
       return;
-    } else if (x._ === "string") {
+    } else if (typeof x === "string") {
       const index = lastIndex++;
       for (let i = index, l = childNodes.length; i < l; i++) {
         let n = childNodes[i];
@@ -54,9 +57,9 @@ const mkpith = (
           return;
         }
       }
-      elm.insertBefore(document.createTextNode(x.v), childNodes[index]);
+      elm.insertBefore(document.createTextNode(x), childNodes[index]);
     } else if (x._ === "reduce") {
-      o(x.g);
+      o((s) => x.g(s));
     } else if (x._ === "elm") {
       const index = lastIndex++;
       for (let i = index, l = childNodes.length; i < l; i++) {
@@ -87,7 +90,7 @@ const bark = mkpith((r) => {
   if (os !== state) console.info(state);
 }, rootNode);
 
-bark(fun(counter));
+bark(counter);
 
 // function page1(o, p) {
 //   o("page 1");
