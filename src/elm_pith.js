c025bff8b96879975a3f29ae56fa036e3041f957
Archil Bolkvadze <a.bolkvadze@gmail.com>
2020-08-09 15:36:19 +0400

    extract api types from ununotated implementatiton

--- src/elm_pith.js
+++ src/elm_pith.js
@@ -10,49 +10,9 @@ export type O =
     |};
 export function empty<T>(_: T) {}
 export function mkElementPith(elm: Element): (O) => void {
-  var count = 0;
-  const { childNodes } = elm;
-  const childPiths: Array<(O) => void> = [];
-  return function pith(x: O): void {
-    if (x == null) {
-      for (let l = childNodes.length; l > count; l--) {
-        elm.removeChild(childNodes[count]);
-        childPiths.splice(count, 1);
-      }
-      count = 0;
-    } else if (typeof x === "string") {
-      const index = count++;
-      for (let i = index, l = childNodes.length; i < l; i++)
-        if (childNodes[i].nodeType === 3 && childNodes[i].textContent === x) {
-          if (index < i) {
-            elm.insertBefore(childNodes[i], childNodes[index]);
-            childPiths.splice(index, 0, ...childPiths.splice(i, 1));
-          }
-          return;
-        }
-      elm.insertBefore(document.createTextNode(x), childNodes[index]);
-      childPiths.splice(index, 0, empty);
-    } else if (x._ === "elm") {
-      const index = count++;
-      for (let i = index, l = childNodes.length; i < l; i++)
-        if (x.eq(childNodes[i])) {
-          if (index < i) {
-            elm.insertBefore(childNodes[i], childNodes[index]);
-            childPiths.splice(index, 0, ...childPiths.splice(i, 1));
-          }
-          const ob = childPiths[index];
-          x.bark(ob);
-          return;
-        }
-      const child = x.ctor();
-      const ob = mkElementPith(child);
-      elm.insertBefore(child, childNodes[index]);
-      childPiths.splice(index, 0, ob);
-      x.bark(ob);
-    } else {
-      (x: empty);
-      throw new Error("x not empty");
-    }
+  const ob = mkBasePith(elm);
+  return function pith(x) {
+    ob(x);
   };
 }
 
@@ -107,3 +67,50 @@ export const reduce = <S>(r: (S) => S): ({| _: "reduce", r: (S) => S |}) => ({
   _: ("reduce": "reduce"),
   r,
 });
+
+function mkBasePith(elm) {
+  var count = 0;
+  const { childNodes } = elm;
+  const childPiths = [];
+  return function pith(x): void {
+    if (x == null) {
+      for (let l = childNodes.length; l > count; l--) {
+        elm.removeChild(childNodes[count]);
+        childPiths.splice(count, 1);
+      }
+      count = 0;
+    } else if (typeof x === "string") {
+      const index = count++;
+      for (let i = index, l = childNodes.length; i < l; i++)
+        if (childNodes[i].nodeType === 3 && childNodes[i].textContent === x) {
+          if (index < i) {
+            elm.insertBefore(childNodes[i], childNodes[index]);
+            childPiths.splice(index, 0, ...childPiths.splice(i, 1));
+          }
+          return;
+        }
+      elm.insertBefore(document.createTextNode(x), childNodes[index]);
+      childPiths.splice(index, 0, empty);
+    } else if (x._ === "elm") {
+      const index = count++;
+      for (let i = index, l = childNodes.length; i < l; i++)
+        if (x.eq(childNodes[i])) {
+          if (index < i) {
+            elm.insertBefore(childNodes[i], childNodes[index]);
+            childPiths.splice(index, 0, ...childPiths.splice(i, 1));
+          }
+          const ob = childPiths[index];
+          x.bark(ob);
+          return;
+        }
+      const child = x.ctor();
+      const ob = mkElementPith(child);
+      elm.insertBefore(child, childNodes[index]);
+      childPiths.splice(index, 0, ob);
+      x.bark(ob);
+    } else {
+      (x: empty);
+      throw new Error("x not empty");
+    }
+  };
+}
