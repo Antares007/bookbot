6d3fc29677105b839086306aaf69241f5071cc4d
Archil Bolkvadze <a.bolkvadze@gmail.com>
2020-08-22 13:44:18 +0400

    ob

--- src/counter7.js
+++ src/counter7.js
@@ -5,39 +5,52 @@ const { dispose, element, makeElementPith } = E;
 const C = (i: number) => {
   var n = 0;
   var op;
-  return element(
-    "div",
-    (o) => {
-      element("button", (o, elm) => {
-        const listener = (e: MouseEvent) => {
-          n++;
-          op((o) => o(n + ""));
-        };
-        elm.addEventListener("click", listener);
-        dispose(() => elm.removeEventListener("click", listener))(o);
-        o("+");
-        if (i > 0) C(i - 1)(o);
-      })(o);
-      element("button", (o, elm) => {
-        const listener = (e: MouseEvent) => {
-          n--;
-          op((o) => o(n + ""));
-        };
-        elm.addEventListener("click", listener);
-        dispose(() => elm.removeEventListener("click", listener))(o);
-        o("-");
-        if (i > 0) C(i - 1)(o);
-      })(o);
-      element("div", (o) => {
-        op = o;
-        o(n + "");
-      })(o);
-    },
-    "C" + i
-  );
+  return (ob) =>
+    element(
+      "div",
+      (o) => {
+        element("button", (o, elm) => {
+          const listener = (e: MouseEvent) => {
+            ob(1);
+            n++;
+            op((o) => o(n + ""));
+          };
+          elm.addEventListener("click", listener);
+          dispose(() => elm.removeEventListener("click", listener))(o);
+          o("+");
+          if (i > 0)
+            C(i - 1)((x: E.O | number) =>
+              typeof x === "number" ? ob(x) : o(x)
+            );
+        })(o);
+        element("button", (o, elm) => {
+          const listener = (e: MouseEvent) => {
+            ob(-1);
+            n--;
+            op((o) => o(n + ""));
+          };
+          elm.addEventListener("click", listener);
+          dispose(() => elm.removeEventListener("click", listener))(o);
+          o("-");
+          if (i > 0)
+            C(i - 1)((x: E.O | number) =>
+              typeof x === "number" ? ob(x) : o(x)
+            );
+        })(o);
+        element("div", (o) => {
+          op = o;
+          o(n + "");
+        })(o);
+      },
+      "C" + i
+    )(ob);
 };
 const o = makeElementPith((document.body = document.createElement("body")));
-o(C(1));
+o((o) =>
+  C(1)((x: E.O | number) =>
+    typeof x === "number" ? console.log(x) : o((x: E.O))
+  )
+);
 Object.assign(window, {
   o,
   element,
