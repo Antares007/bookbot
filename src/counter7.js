38510d70825f7ca223fdb74c52d7209c61415d1f
Archil Bolkvadze <a.bolkvadze@gmail.com>
2020-08-24 13:48:35 +0400

    sync

--- src/counter7.js
+++ src/counter7.js
@@ -3,14 +3,11 @@ import { static_cast } from "./static_cast.js";
 import * as E from "./elm_pith";
 import type { O, P, N, N1 } from "./elm_pith";
 const { dispose, element, makeElementPith } = E;
-function button<S, T>(nar: N<ORA<S, T>>, n: T): N<Relement<S, T>> {
-  return relement("button", (o, elm) => {
-    const listener = () => action(n)(o);
-    elm.addEventListener("click", listener);
-    dispose(() => elm.removeEventListener("click", listener))(o);
-    nar(o);
-  });
-}
+
+type ORA<S = *, T = *> = O | Relement<S> | R<S> | A<T> | On<S, T>;
+type On<S, T> = {| _: "on", v: N1<ORA<S, T>, A<T>> |};
+type A<+T> = {| _: "a", +v: T |};
+type R<S = *> = {| _: "r", v: (S) => S |};
 type Relement<S = *, T = *> = {|
   _: "relm",
   v: {
@@ -19,10 +16,10 @@ type Relement<S = *, T = *> = {|
     nar: N1<ORA<S, T>, Element>,
   },
 |};
-type ORA<S = *, T = *> = O | Relement<S> | R<S> | A<T>;
-type A<T> = {| _: "a", v: T |};
-type R<S = *> = {| _: "r", v: (S) => S |};
-
+function on<S, T>(v: N1<ORA<S, T>, A<T>>): N<On<S, T>> {
+  const on = { _: "on", v };
+  return (o) => o(on);
+}
 function action<T>(v: T): N<A<T>> {
   const a = { _: "a", v };
   return (o) => o(a);
@@ -48,9 +45,13 @@ function reduce<S>(v: (S) => S): N<R<S>> {
 }
 function ring<S, T>(or: P<R<S>>, oa: P<A<T>>): (N<ORA<S, T>>) => N<O> {
   return (nar) => (o) => {
+    const ons = [];
     nar(function pith(x) {
       if (x == null || typeof x !== "object") {
+        ons.splice(0, ons.length);
         o(x);
+      } else if (x._ === "on") {
+        ons.push(x);
       } else if (x._ === "a") {
         oa(x);
       } else if (x._ === "r") {
@@ -58,7 +59,11 @@ function ring<S, T>(or: P<R<S>>, oa: P<A<T>>): (N<ORA<S, T>>) => N<O> {
       } else if (x._ === "relm") {
         element(
           x.v.tag,
-          (o, elm) => ring(or, oa)((o) => x.v.nar(o, elm))(o),
+          (o, elm) =>
+            ring(or, (a) => {
+              if (ons.length) ons.forEach((n) => n.v(pith, a));
+              else oa(a);
+            })((o) => x.v.nar(o, elm))(o),
           x.v.key
         )(o);
       } else {
@@ -67,7 +72,14 @@ function ring<S, T>(or: P<R<S>>, oa: P<A<T>>): (N<ORA<S, T>>) => N<O> {
     });
   };
 }
-
+function button<S, T>(nar: N<ORA<S, T>>, n: T): N<Relement<S, T>> {
+  return relement("button", (o, elm) => {
+    const listener = () => action(n)(o);
+    elm.addEventListener("click", listener);
+    dispose(() => elm.removeEventListener("click", listener))(o);
+    nar(o);
+  });
+}
 function C(
   depth: number = 0,
   key: string = "C",
@@ -75,23 +87,32 @@ function C(
 ): N<ORA<{| n: number |}, *>> {
   var n = 0;
   var op;
-  return relement(
-    "div",
-    (o) => {
-      button((o) => {
-        o("+");
-        depth > 0 && C(depth - 1, key, init)(o);
-      }, 1)(o);
-      relement("div", (o) => {
-        op = o;
-        reduce((s) => {
-          o(s.n + "");
-          return s;
+  return (ob) =>
+    relement(
+      "div",
+      (o) => {
+        button((o) => {
+          o("+");
+          depth > 0 && C(depth - 1, key, init)(o);
+        }, 1)(o);
+        on((o, a) => {
+          reduce((s) => ({ n: s.n + 1 }))(o);
+          reduce((s) => {
+            op(s.n + "");
+            return s;
+          })(op);
+          op();
         })(o);
-      })(o);
-    },
-    "C" + depth
-  );
+        relement("div", (o) => {
+          op = o;
+          reduce((s) => {
+            o(s.n + "");
+            return s;
+          })(o);
+        })(o);
+      },
+      "C" + depth
+    )(ob);
 }
 var state = { n: 9 };
 const o = makeElementPith((document.body = document.createElement("body")));
@@ -105,9 +126,33 @@ const addstate = ring(
 o(addstate(C(1)));
 
 Object.assign(window, {
+  ls() {
+    console.log(state);
+  },
   o,
   element,
   dispose,
   makeElementPith,
   C,
 });
+function rmap<A: { ... }, B, T>(
+  key: string,
+  b: B
+): (P<ORA<A, T>>) => P<ORA<B, T>> {
+  return (o) => (x) => {
+    if (x == null || typeof x !== "object") {
+      o(x);
+    } else if (x._ === "relm") {
+      x;
+    } else if (x._ === "on") {
+      x;
+    } else if (x._ === "r")
+      reduce((a) => {
+        const ob = a[key] || b;
+        const nb = x.v(ob);
+        if (ob === nb) return a;
+        return { ...a, [key]: nb };
+      })(o);
+    else o(x);
+  };
+}
