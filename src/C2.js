b809d620a8c162c15bdf823a4cae5f052ee068da
Archil Bolkvadze <a.bolkvadze@gmail.com>
2020-09-04 12:26:08 +0400

    remove extra

--- src/C2.js
+++ src/C2.js
@@ -4,15 +4,7 @@ import * as E from "./E.js";
 
 var state = { n: 0 };
 
-const o = E.make(
-  (r) => {
-    const oldstate = state;
-    state = r.v(state);
-    if (state !== oldstate) console.info(state);
-  },
-  (v) => console.log(v),
-  (document.body = document.createElement("body"))
-);
+const o = E.make((document.body = document.createElement("body")));
 
 const aelement = E.element(api);
 
@@ -26,7 +18,6 @@ const C = (d: number = 2) =>
       o("-");
       d > 0 && o(C(d - 1));
     });
-    o((s) => s);
     o("0");
   });
 
@@ -37,11 +28,9 @@ Object.assign(window, { o, C, E, api, aelement });
 function api(nar) {
   return (o) => {
     nar((r, s, t) => {
-      if (typeof r === "function") {
-        o({ t: "reduce", v: r });
-      } else if ("string" === typeof r) {
+      if ("string" === typeof r) {
         if (s) o(E.element(api)(r, s, t));
-        else o({ t: "string", v: r });
+        else o({ t: "text", v: r });
       } else {
         o(r);
       }
--- src/E.js
+++ src/E.js
@@ -1,53 +1,35 @@
 // @flow strict
 import type { P, N } from "./NP.js";
-export type o_t<S, V> =
-  | value_t<V>
-  | reduce_t<S>
-  | action_t
-  | end_t
-  | string_t
-  | element_t<S, V>;
-export type string_t = {| t: "string", v: string |};
+
+export type o_t = action_t | end_t | string_t | element_t;
+
+export type string_t = {| t: "text", v: string |};
 export type end_t = {| t: "end", v?: void |};
 export type action_t = {| t: "action", v: (Element) => ?() => void |};
-export type value_t<+V> = {| t: "value", +v: V |};
-export type reduce_t<S> = {| t: "reduce", +v: (S) => S |};
-export type element_t<S, V> = {|
-  t: "element",
-  v: { sel: string, nar: N<o_t<S, V>>, key?: ?string },
-|};
+export type element_t = {| t: "element", v: t0_t |};
+
+type t0_t = {| sel: string, nar: N<o_t>, key?: ?string |};
+
 export const end: end_t = { t: "end" };
-export function element<S, V, T>(
-  pmap: (T) => N<o_t<S, V>>
-): (string, T, ?string) => element_t<S, V> {
+export function element<T>(
+  pmap: (T) => N<o_t>
+): (string, T, ?string) => element_t {
   return (sel, t, key) => {
     return { t: "element", v: { sel, nar: pmap(t), key } };
   };
 }
 export function text(v: string): string_t {
-  return { t: "string", v };
+  return { t: "text", v };
 }
 export function action(v: (Element) => ?() => void): action_t {
   return { t: "action", v };
 }
-export function value<V>(v: V): value_t<V> {
-  return { t: "value", v };
-}
-export function reduce<S>(v: (S) => S): reduce_t<S> {
-  return { t: "reduce", v };
-}
-export function make<S, V>(
-  ro: P<reduce_t<S>>,
-  vo: P<value_t<V>>,
-  elm: Element,
-  depth: number = 0
-): P<o_t<S, V>> {
+export function make(elm: Element, depth: number = 0): P<o_t> {
   var childs_count = 0;
   const { childNodes } = elm;
-  const childPiths: Array<?P<o_t<S, V>>> = [];
+  const childPiths: Array<?P<o_t>> = [];
   var actions_count = 0;
   const actions: Array<[action_t, ?() => void]> = [];
-  var prev;
   return function pith(x) {
     if ("element" === x.t) {
       const index = childs_count++;
@@ -76,7 +58,7 @@ export function make<S, V>(
             ob({ t: "end" });
             return;
           }
-          ob = make(ro, vo, n, depth + 1);
+          ob = make(n, depth + 1);
           childPiths.splice(index, 0, ob);
           nar(ob);
           ob({ t: "end" });
@@ -87,11 +69,11 @@ export function make<S, V>(
       if (id) child.id = id;
       for (let str of classList) child.classList.add(str);
       elm.insertBefore(child, childNodes[index]);
-      const ob = make(ro, vo, child, depth + 1);
+      const ob = make(child, depth + 1);
       childPiths.splice(index, 0, ob);
       nar(ob);
       ob({ t: "end" });
-    } else if ("string" === x.t) {
+    } else if ("text" === x.t) {
       const index = childs_count++;
       const l = childNodes.length;
       for (let i = index; i < l; i++)
@@ -104,10 +86,6 @@ export function make<S, V>(
         }
       elm.insertBefore(document.createTextNode(x.v), childNodes[index]);
       childPiths.splice(index, 0, null);
-    } else if ("reduce" === x.t) {
-      ro(x);
-    } else if ("value" === x.t) {
-      vo(x);
     } else if ("action" === x.t) {
       const index = actions_count++;
       const l = actions.length;
@@ -138,39 +116,14 @@ export function make<S, V>(
     }
   };
 }
-export function mmap<A, B>(f: (A) => B): (?A) => ?B {
-  return (x) => (x ? f(x) : null);
-}
-export function rmap<A: { ... }, B, V>(
-  key: string,
-  b: B
-): (N<o_t<B, V>>) => N<o_t<A, V>> {
-  return (nar) => (o) => {
-    nar(function rmap_pith(x) {
-      if ("reduce" === x.t) {
-        o({
-          t: "reduce",
-          v: (a) => {
-            const oldb = a[key] || b;
-            const newb = x.v(oldb);
-            if (oldb === newb) return a;
-            return { ...a, [key]: newb };
-          },
-        });
-      } else if ("element" === x.t) {
-        o({ ...x, v: { ...x.v, nar: rmap(key, b)(x.v.nar) } });
-      } else {
-        o(x);
-      }
-    });
-  };
-}
-function hashString(s) {
+function hashString(s: string): number {
   for (var i = 0, h = 0; i < s.length; i++)
     h = (Math.imul(31, h) + s.charCodeAt(i)) | 0;
   return h;
 }
-function parseSelector(sel) {
+function parseSelector(
+  sel: string
+): {| tag: string, classList: Array<string>, id: ?string |} {
   const classList = [];
   const l = sel.length;
   var tag = "";
@@ -182,7 +135,10 @@ function parseSelector(sel) {
   while (i < l) {
     tmp = "";
     if (sel[i] === ".") {
-      while (++i < l && sel[i] !== "." && sel[i] !== "#") tmp += sel[i];
+      while (++i < l && sel[i] !== "." && sel[i] !== "#") {
+        let see: string = sel[i];
+        tmp += see;
+      }
       if (tmp.length) classList.push(tmp);
     } else if (sel[i] === "#") {
       while (++i < l && sel[i] !== "." && sel[i] !== "#") tmp += sel[i];
