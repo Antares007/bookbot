1badb27693d61208817313a1aece2b3649f70cb2
Archil Bolkvadze <a.bolkvadze@gmail.com>
2020-09-05 11:35:09 +0400

    define R

--- src/C2.js
+++ src/C2.js
@@ -1,19 +1,20 @@
 // @flow strict
 import type { P, N } from "./NP";
 import * as E from "./E.js";
+import * as R from "./R.js";
 
 const C = (r, d: number = 2) =>
-  E.element(reduce<{ n: number }>(r))("div", (o) => {
+  E.element(R.reduce<{ n: number }>(r))("div", (o) => {
     var op;
     o("button", (o) => {
-      o(on("click")((e) => (o((s) => ({ ...s, n: s.n + 1 })), redraw())));
+      o(R.on("click")(() => (o((s) => ({ ...s, n: s.n + 1 })), redraw())));
       o("+");
-      d > 0 && o(C(rmap(r)("+", { n: 0 }), d - 1));
+      d > 0 && o(C(R.rmap(r)("+", { n: 0 }), d - 1));
     });
     o("button", (o) => {
-      o(on("click")((e) => (o((s) => ({ ...s, n: s.n - 1 })), redraw())));
+      o(R.on("click")(() => (o((s) => ({ ...s, n: s.n - 1 })), redraw())));
       o("-");
-      d > 0 && o(C(rmap(r)("-", { n: 0 }), d - 1));
+      d > 0 && o(C(R.rmap(r)("-", { n: 0 }), d - 1));
     });
     const redraw = () => {
       o((s) => (op(s.n + ""), s));
@@ -37,51 +38,4 @@ o(
 );
 o({ t: "end" });
 
-Object.assign(window, { o, C, E, str_api });
-
-type reduce_api_pith_t<S> = (
-  ((S) => S) | string | E.o_t,
-  void | ((reduce_api_pith_t<S>) => void),
-  void | string
-) => void;
-function reduce<S>(
-  or: P<(S) => S>
-): ((reduce_api_pith_t<S>) => void) => N<E.o_t> {
-  return (nar) =>
-    str_api((o) =>
-      nar((r, s, t) =>
-        "function" === typeof r ? or(r) : o(r, s ? reduce(or)(s) : s, t)
-      )
-    );
-}
-function rmap<A: { ... }, B>(o: P<(A) => A>): (string, B) => P<(B) => B> {
-  return (key, init) => (rb) =>
-    o((a) => {
-      const ob = a[key] || init;
-      const nb = rb(ob);
-      if (ob === nb) return a;
-      return { ...a, [key]: nb };
-    });
-}
-type str_api_pith_t = (
-  string | E.o_t,
-  void | ((str_api_pith_t) => void),
-  void | string
-) => void;
-function str_api(nar: (str_api_pith_t) => void): N<E.o_t> {
-  return (o) =>
-    nar((r, s, t) =>
-      "string" === typeof r
-        ? s
-          ? o(E.element(str_api)(r, s, t))
-          : o(E.text(r))
-        : o(r)
-    );
-}
-function on(type) {
-  return (l: EventHandler) =>
-    E.action((elm) => {
-      elm.addEventListener(type, l);
-      return () => elm.removeEventListener(type, l);
-    });
-}
+Object.assign(window, { o, C, E, R });
---
+++ src/R.js
@@ -0,0 +1,52 @@
+// @flow strict
+import type { P, N } from "./NP";
+import * as E from "./E.js";
+
+export type reduce_api_pith_t<S> = (
+  ((S) => S) | string | E.o_t,
+  void | ((reduce_api_pith_t<S>) => void),
+  void | string
+) => void;
+export function reduce<S>(
+  or: P<(S) => S>
+): ((reduce_api_pith_t<S>) => void) => N<E.o_t> {
+  return (nar) =>
+    str_api((o) =>
+      nar((r, s, t) =>
+        "function" === typeof r ? or(r) : o(r, s ? reduce(or)(s) : s, t)
+      )
+    );
+}
+export function rmap<A: { ... }, B>(
+  o: P<(A) => A>
+): (string, B) => P<(B) => B> {
+  return (key, init) => (rb) =>
+    o((a) => {
+      const ob = a[key] || init;
+      const nb = rb(ob);
+      if (ob === nb) return a;
+      return { ...a, [key]: nb };
+    });
+}
+export type str_api_pith_t = (
+  string | E.o_t,
+  void | ((str_api_pith_t) => void),
+  void | string
+) => void;
+export function str_api(nar: (str_api_pith_t) => void): N<E.o_t> {
+  return (o) =>
+    nar((r, s, t) =>
+      "string" === typeof r
+        ? s
+          ? o(E.element(str_api)(r, s, t))
+          : o(E.text(r))
+        : o(r)
+    );
+}
+export function on(type: string): (EventHandler) => E.action_t {
+  return (l: EventHandler) =>
+    E.action((elm) => {
+      elm.addEventListener(type, l);
+      return () => elm.removeEventListener(type, l);
+    });
+}
