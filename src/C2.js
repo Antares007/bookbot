6ab3f842001a690401e4114c41f4d16c3d400f6e
Archil Bolkvadze <a.bolkvadze@gmail.com>
2020-09-05 11:47:21 +0400

    clean C2

--- src/C2.js
+++ src/C2.js
@@ -5,7 +5,6 @@ import * as R from "./R.js";
 
 const C = (r, d: number = 2) =>
   E.element(R.reduce<{ n: number }>(r))("div", (o) => {
-    var op;
     o("button", (o) => {
       o(R.on("click")(() => (o((s) => ({ ...s, n: s.n + 1 })), redraw())));
       o("+");
@@ -16,18 +15,15 @@ const C = (r, d: number = 2) =>
       o("-");
       d > 0 && o(C(R.rmap(r)("-", { n: 0 }), d - 1));
     });
-    const redraw = () => {
-      o((s) => (op(s.n + ""), s));
-      op(E.end);
-    };
+    var op;
     o("div", (o) => {
       op = o;
-      o((s) => (o(s.n + ""), s));
     });
+    const redraw = () => (o((s) => (op(s.n + ""), s)), op(E.end));
+    redraw();
   });
 
 const o = E.make((document.body = document.createElement("body")));
-
 var state = { n: 0 };
 o(
   C((r) => {
@@ -37,5 +33,4 @@ o(
   })
 );
 o({ t: "end" });
-
 Object.assign(window, { o, C, E, R });
