734452adfa9a19093f04fc21f3a5704b7ba48f62
Archil Bolkvadze <a.bolkvadze@gmail.com>
2020-09-07 14:14:24 +0400

    rvalues

--- src/R.js
+++ src/R.js
@@ -28,6 +28,45 @@ export function rmap<A: { ... }, B>(
       return { ...a, [key]: nb };
     });
 }
+x_api((o) => {
+  o("button", (o) => {
+    o("on", "click", (e: MouseEvent) => {});
+    o("+");
+  });
+  o("button", (o) => {
+    o("on", "click", (e: MouseEvent) => {});
+    o("-");
+  });
+
+  o("a", "value", "11");
+  o("s", "backgraund", "white");
+  o("s", "backgraund", "white");
+});
+export type x_api_pith_t = (
+  string | E.o_t,
+  void | P<x_api_pith_t> | string,
+  void | string | ((Event) => void) | ((MouseEvent) => void)
+) => void;
+export function x_api(nar: (x_api_pith_t) => void): N<E.o_t> {
+  return str_api((o) => {
+    nar((r, s, t) => {
+      if ("on" === r && "string" === typeof s && "function" === typeof t) {
+        r, s, t;
+      } else if ("a" === r && "string" === typeof s && "string" === typeof t) {
+        r, s, t;
+      } else if ("s" === r && "string" === typeof s && "string" === typeof t) {
+        r, s, t;
+      } else if ("string" === typeof s || "function" === typeof t) {
+        o(
+          r,
+          "string" === typeof s ? (o) => o(s) : s ? x_api(s) : s,
+          "function" === typeof t ? t.toString() : t
+        );
+      } else o(r, s ? x_api(s) : s, t);
+    });
+  });
+}
+
 export type str_api_pith_t = (
   string | E.o_t,
   void | ((str_api_pith_t) => void),
@@ -35,13 +74,17 @@ export type str_api_pith_t = (
 ) => void;
 export function str_api(nar: (str_api_pith_t) => void): N<E.o_t> {
   return (o) =>
-    nar((r, s, t) =>
-      "string" === typeof r
-        ? s
-          ? o(E.element(str_api)(r, s, t))
-          : o(E.text(r))
-        : o(r)
-    );
+    nar(function pith(r, s, t) {
+      if ("string" === typeof r) {
+        if (s) o(E.element(str_api)(r, s, t));
+        else if (t) o(E.text(r + t));
+        else o(E.text(r));
+      } else {
+        o(r);
+        s && o(E.text(s.toString()));
+        t && o(E.text(t));
+      }
+    });
 }
 export function on(type: string): (EventHandler) => E.action_t {
   return (l: EventHandler) =>
