22bd8ad6e03ce684b1ec9e278fe1e73d6c98d70c
Archil Bolkvadze <a.bolkvadze@gmail.com>
2020-10-17 17:37:40 +0400

    import ast types from @babel/types

--- examples/a.js
+++ examples/a.js
@@ -3,36 +3,27 @@ import type { N } from "../src/purry";
 import type { rring_rays_t } from "../src/a";
 const a = require("../src/a");
 const ast = require("../lib/babel");
-var state = ast.parse("a+b*o");
+var state = ast.parse("const a=1,b=1,o=2;a+b*o;");
 
 const b = a.bark((r) => {
   state = r(state);
 }, (document.body = document.body || document.createElement("body")));
-b(File);
 
-function div<S>(
-  nar: N<N<rring_rays_t<S>>>,
-  key?: ?string
-): {|
-  m: "relement",
-  a: {| tag: string, nar: N<N<rring_rays_t<S>>>, key?: ?string |},
-|} {
-  return { m: ("relement": "relement"), a: { tag: "div", nar, key } };
-}
-function reduce<S>(a: (S) => S): {| m: "reduce", a: (S) => S |} {
-  return { m: ("reduce": "reduce"), a };
-}
-function DebuggerStatement(o: N<rring_rays_t<ast.DebuggerStatement>>) {}
-function BlockStatement(o: N<rring_rays_t<ast.BlockStatement>>) {}
-function NotImplemented<T>(o: N<rring_rays_t<T>>) {
+const File = make<ast.BNFile>((o) => {
   o(
-    reduce((s) => {
-      console.log("NotImplemented", s);
-      return s;
-    })
+    div(
+      map(
+        (a) => a.program,
+        (a, b) => ({ ...a, program: b })
+      )(Program),
+      "program"
+    )
   );
-}
-function BinaryExpression(o: N<rring_rays_t<ast.BinaryExpression>>) {
+});
+
+const DebuggerStatement = make<ast.BNDebuggerStatement>((o) => {});
+const BlockStatement = make<ast.BNBlockStatement>((o) => {});
+const BinaryExpression = make<ast.BNBinaryExpression>((o) => {
   //{ left: Expression,
   //  operator: ( "+" | "-" | "/" | "%" | "*" | "**" | "&" | "|" | ">>" | ">>>" | "<<" | "^" | "==" | "===" | "!=" | "!==" | "in" | "instanceof" | ">" | "<" | ">=" | "<="),
   //  right: Expression,
@@ -45,58 +36,65 @@ function BinaryExpression(o: N<rring_rays_t<ast.BinaryExpression>>) {
   //    return s;
   //  })
   //);
-}
-function kdiv<S: { ... }, K>(
-  key: string,
-  nar: N<N<rring_rays_t<K>>>
-): N<N<rring_rays_t<S>>> {
-  return (o) =>
-    o(
-      div(
-        a.ring((r) => o(reduce((s) => ({ ...s, [key]: r(s[key]) }))))(nar),
-        key
-      )
-    );
-}
-function Expression(o: N<rring_rays_t<ast.Expression>>) {}
-function ExpressionStatement(o: N<rring_rays_t<ast.ExpressionStatement>>) {
-  o(
-    reduce((s: ast.ExpressionStatement) => {
-      kdiv(
-        "expression",
-        s.expression.type === "BinaryExpression"
-          ? BinaryExpression
-          : NotImplemented
-      )(o);
-
-      return s;
-    })
-  );
-}
-
-function Program(o: N<rring_rays_t<ast.Program>>) {
+});
+const Expression = make<ast.BNExpression>((o) => {});
+const ExpressionStatement = make<ast.BNExpressionStatement>((o) => {
   o(
     div(
-      omap(
-        "body",
-        ([]: $PropertyType<ast.Program, "body">)
+      map(
+        (a) => a.expression,
+        (a, b) => ({ ...a, expression: b })
+      )((o) => {
+        o(
+          reduce((s) => {
+            if (s.type === "BinaryExpression")
+              map(
+                (a) => (a.type === "BinaryExpression" ? a : s),
+                (a, b) => b
+              )(BinaryExpression);
+            else o;
+            return s;
+          })
+        );
+      }),
+      "expression"
+    )
+  );
+});
+
+const Program = make<ast.BNProgram>((o) => {
+  o(
+    div(
+      map(
+        (a) => a.body,
+        (a, b) => ({ ...a, body: b })
       )((o) =>
         o(
           reduce((s) => {
-            s.forEach((n, i) =>
+            s.forEach((n, i) => {
+              const merge = (a, b) => a.map((n, j) => (j === i ? b : n));
               o(
                 div(
                   n.type === "BlockStatement"
-                    ? amap(i, n)(BlockStatement)
+                    ? map(
+                        (a) => (a[i].type === "BlockStatement" ? a[i] : n),
+                        merge
+                      )(BlockStatement)
                     : n.type === "DebuggerStatement"
-                    ? amap(i, n)(DebuggerStatement)
+                    ? map(
+                        (a) => (a[i].type === "DebuggerStatement" ? a[i] : n),
+                        merge
+                      )(DebuggerStatement)
                     : n.type === "ExpressionStatement"
-                    ? amap(i, n)(ExpressionStatement)
-                    : (o) => {},
+                    ? map(
+                        (a) => (a[i].type === "ExpressionStatement" ? a[i] : n),
+                        merge
+                      )(ExpressionStatement)
+                    : map((a) => a[i], merge)(NotImplemented),
                   i + ""
                 )
-              )
-            );
+              );
+            });
             return s;
           })
         )
@@ -104,68 +102,83 @@ function Program(o: N<rring_rays_t<ast.Program>>) {
       "body"
     )
   );
+});
+
+b(File);
+
+// ***************************************
+// ***************************************
+// ***************************************
+
+function make<S: ast.BN>(nar: N<N<rring_rays_t<S>>>): N<N<rring_rays_t<S>>> {
+  return (o) => {
+    o({
+      m: "reduce",
+      a(s) {
+        o({
+          m: "get",
+          a(elm) {
+            elm.className = s.type;
+          },
+        });
+        nar(o);
+        return s;
+      },
+    });
+  };
+}
+function map<A, B>(
+  extract: (A) => B,
+  merge: (A, B) => A
+): (N<N<rring_rays_t<B>>>) => N<N<rring_rays_t<A>>> {
+  return (nar) => (o) =>
+    a.ring((r) =>
+      o(
+        reduce((a) => {
+          const ob = extract(a);
+          const nb = r(ob);
+          return ob === nb ? a : merge(a, nb);
+        })
+      )
+    )(nar)(o);
+}
+function omap<A: { ... }, B>(
+  key: string,
+  assert: (mixed) => B
+): (N<N<rring_rays_t<B>>>) => N<N<rring_rays_t<A>>> {
+  return (nar) =>
+    map(
+      (a) => assert(a[key]),
+      (a, b) => ({ ...a, [key]: b })
+    )(nar);
+}
+function amap<A, B>(
+  i: number,
+  extract: (A | B) => B
+): (N<N<rring_rays_t<B>>>) => N<N<rring_rays_t<Array<A | B>>>> {
+  return (nar) =>
+    map(
+      (a) => extract(a[i]),
+      (a, b) => a.map((n, j) => (j === i ? b : n))
+    )(nar);
+}
+function NotImplemented<T: { +type: string }>(o: N<rring_rays_t<T>>) {
   o(
     reduce((s) => {
-      s.body.forEach((n, i) => {
-        o(
-          div(
-            n.type === "BlockStatement"
-              ? a.ring((r) =>
-                  o(
-                    reduce((s) => ({
-                      ...s,
-                      body: s.body.map((s, k) =>
-                        k !== i ? s : r(s.type === n.type ? s : n)
-                      ),
-                    }))
-                  )
-                )(BlockStatement)
-              : n.type === "DebuggerStatement"
-              ? a.ring((r) =>
-                  o(
-                    reduce((s) => ({
-                      ...s,
-                      body: s.body.map((s, k) =>
-                        k !== i ? s : r(s.type === n.type ? s : n)
-                      ),
-                    }))
-                  )
-                )(DebuggerStatement)
-              : (o) => {},
-            "" + i
-          )
-        );
-      });
+      console.log("NotImplemented", s.type, s);
       return s;
     })
   );
 }
-function File(o: N<rring_rays_t<ast.File>>) {
-  o(
-    div(
-      omap("program", { type: "Program", sourceType: "module", body: [] })(
-        Program
-      ),
-      "program"
-    )
-  );
+function div<S>(
+  nar: N<N<rring_rays_t<S>>>,
+  key: string
+): {|
+  m: "relement",
+  a: {| tag: string, nar: N<N<rring_rays_t<S>>>, key?: ?string |},
+|} {
+  return { m: ("relement": "relement"), a: { tag: "div", nar, key } };
 }
-
-function omap<S: { ... }, K>(
-  key: string,
-  init: K
-): (N<N<rring_rays_t<K>>>) => N<N<rring_rays_t<S>>> {
-  return (nar) => (o) =>
-    a.ring((r) => o(reduce((s) => ({ ...s, [key]: r(s[key] || init) }))))(nar)(
-      o
-    );
-}
-function amap<S, K>(
-  index: number,
-  init: K
-): (N<N<rring_rays_t<K>>>) => N<N<rring_rays_t<Array<S>>>> {
-  return (nar) => (o) => {};
-  //a.ring((r) => o(reduce((s) => ({ ...s, [key]: r(s[key] || init) }))))(nar)(
-  //  o
-  //);
+function reduce<S>(a: (S) => S): {| m: "reduce", a: (S) => S |} {
+  return { m: ("reduce": "reduce"), a };
 }
