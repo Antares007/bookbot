5ce125594ec9d47bb18d561993c3e2bb3d78cd90
Archil Bolkvadze <a.bolkvadze@gmail.com>
2020-11-06 13:03:18 +0400

    clean

--- src/abc.js
+++ src/abc.js
@@ -1,53 +1,17 @@
 // @flow strict
 const { bexp } = require("./bexp");
 const { static_cast } = require("./utils/static_cast");
-export type n3<OT: {} = {}> = {
-  N: null[],
-  a: mixed[][],
-  n: number[],
-  s: string[],
-  b: boolean[],
-  u: void[],
-  o: OT[],
-};
+
 export function A(f: mixed, ...aies: Array<mixed>): mixed {
-  var args = [];
-  for (let a of aies) {
+  var args = [f];
+  for (let a of aies)
     if (a !== null && typeof a === "object" && typeof a.type === "string")
-      if (aies.length === 1)
-        return static_cast<(mixed) => void>(f).call(this, a);
-      else
-        Object.keys(a).forEach((k) => {
-          args = args.concat(a[k]);
-        });
+      if (aies.length === 1) return static_cast<(mixed) => void>(f)(a);
+      else for (let k of Object.keys(a)) args = args.concat(a[k]);
     else args.push(a);
-  }
-  return C.call(this, f, ...args);
+  return C.apply(null, args);
 }
-export function C(f: mixed, ...args: Array<mixed>): mixed {
-  const proto = { type: "" };
-  const aob = Object.create(proto);
-  for (let a of args) {
-    const tn = (typeof a)[0];
-    if (Array.isArray(aob[tn])) aob[tn].push(a);
-    else aob[tn] = [a];
-  }
-  proto.type = "";
-  for (let tn of "nsbuf")
-    if (aob[tn]?.length)
-      proto.type += tn + (aob[tn].length > 1 ? aob[tn].length : "");
-  if (aob.o?.length) {
-    proto.type += "o[";
-    aob.o.forEach((x, i) => {
-      if (typeof x?.type === "string") proto.type += x.type;
-      else proto.type += "{}";
-      if (i + 1 < aob.o.length) proto.type += ";";
-    });
-    proto.type += "]";
-  }
-  return static_cast<({}) => mixed>(f)(aob);
-}
-export function B(...funs: Array<mixed>): ({}) => mixed {
+export function B(...funs: Array<mixed>): mixed {
   var code = `const funs = arguments[0];\nreturn a => {\n`;
   funs.forEach((f, i) => {
     if (typeof f === "function")
@@ -55,8 +19,31 @@ export function B(...funs: Array<mixed>): ({}) => mixed {
         static_cast<() => void>(f),
         "a"
       )}\n) return funs[${i}].call(a, a);\n`;
-    else throw new Error("arg for B is not a function");
+    else throw new Error(`args[${i}] for B is ${typeof f}`);
   });
   code += `throw new Error('empty');\n}\n`;
   return new Function(code).call(null, funs);
+}
+export function C(f: mixed, ...args: Array<mixed>): mixed {
+  const proto = { type: "" };
+  const abc = Object.create(proto);
+  for (let a of args) {
+    const tn = (typeof a)[0];
+    if (Array.isArray(abc[tn])) abc[tn].push(a);
+    else abc[tn] = [a];
+  }
+  proto.type = "";
+  for (let tn of "nsbuf")
+    if (abc[tn]?.length)
+      proto.type += tn + (abc[tn].length > 1 ? abc[tn].length : "");
+  if (abc.o?.length) {
+    proto.type += "o[";
+    abc.o.forEach((x, i) => {
+      if (typeof x?.type === "string") proto.type += x.type;
+      else proto.type += "{}";
+      if (i + 1 < abc.o.length) proto.type += ";";
+    });
+    proto.type += "]";
+  }
+  return static_cast<({}) => mixed>(f)(abc);
 }
