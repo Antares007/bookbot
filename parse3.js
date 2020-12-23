0fa757d9c2ae51186a6e3d0e1f7503c658dfd5d8
Archil Bolkvadze <a.bolkvadze@gmail.com>
2020-12-23 12:52:26 +0400

    change configuration

--- parse3.js
+++ parse3.js
@@ -33,7 +33,7 @@ const lhsOperationRhs = (lhs, op, rhs) =>
     "/": (l, o, r) => l / r,
   });
 // prettier-ignore
-//const ABO = programmer`
+//const ABO = mn`
 //  S       → ^[\\x20\\n\\r\\t]+
 //  Number  → ^[1-9][0-9]* ${createNumber}
 //  String  → " ^[^"]* " ${createString}
@@ -76,119 +76,127 @@ const lhsOperationRhs = (lhs, op, rhs) =>
 //    console.log(JSON.stringify(c));
 //  }
 //);
-const tdda = ()=>{}
-tdda({
-  [`S → ab            `]: ["ab"],
-  [`S → a | ε         `]: ["a"],
-  [`B → a B | o B | b `]: ["aob", "aaoob", "aaaooob"],
+const tdda = () => {}
+tdd({
+  [`S → ab              `]: ["ab"],
+  [`S → a | ε           `]: ["a"],
+  [`B → a B | o B | b   `]: ["aob", "aaoob", "aaaooob"],
   [`S → A B | B A
     A → a a
-    B → a b           `]: ["aaab", "abaa"],
+    B → a b             `]: ["aaab", "abaa"],
+  [`S → A B | B A
+    C → a
+    A → C a
+    B → C b             `]: ["aaab", "abaa"],
   [`S → A / c b
     A → c B
-    B → a             `]: ["cb", "ca"],
+    B → a               `]: ["cb", "ca"],
   [`C → c S
     A → a S
     B → b S
-    S → A / B / ε     `]: ["cab"],
-  [`S → ^a+ ^b* | ε   `]: ["", "a", "aaabbb", "abbbb"],
+    S → A / B / ε       `]: ["cab"],
+  [`S → ^a+ ^b* | ε     `]: ["", "a", "aaabbb", "abbbb"],
   [`S → b A
-    A → a A | ε       `]: ["baaa"],
+    A → a A | ε         `]: ["baaa"],
   //[`S → S a / b
-  //  A → S a / S b   `]: ["baaa"],
+  //  A → S a / S b     `]: ["baaa"],
   //[`
-  //  A → C a | e
-  //  B → C b
-  //  C → A | B | c   `]: ["cab"],
+  //  A → C a | e |
+  //  B → C b |
+  //  C → A | B | c |   `]: ["cab"],
 });
-// A    C
-// C a  A
-// A    C a
-// C a  A
-// A    C a
-// C a  A
-// A    C a
-// e    c
 
-const _ = (...a) => a.join("");
-
-//const _0	    =(s,o)=>o(s,	_,	  null,	'G')
-//const ε_0	    =(s,o)=>o(s,	"",	  _0,	  'T')
-//const _ε_0	  =(s,o)=>o(s,	_,	  ε_0,	'G')
-//const A_ε_0	  =(s,o)=>o(s,	A,	  _ε_0,	'A')
-//const aA_ε_0	=(s,o)=>o(s,	"a",	A_ε_0,'T')
+//            A                           C
+//            ↓                           ↓
+//            C a | e |                   A | B | c |
+//            ↓                           ↓
+//            A | B | c |                 C a | e |
+//            ↓                           ↓
+//            C a | e |                   A | B | c |
+//            ↓                           ↓
+//            A | B | c |                 C a | e |
+//            ↓                           ↓
+//      C a | e |                 A | B | c |
 //
-//const A_0	    =(s,o)=>o(s,	A,	  _0,	  'A')
-//const bA_0	  =(s,o)=>o(s,	"b",	A_0,	'T')
+//            S
+//            ↓
+//            S a | b |
+//            ↓
+//            S a | b |
+//            ↓
+//            S a | b |
+//            ↓
+//      S a | b |
 
-const c = {
-  stack: [],
-  tails: [],
-  pos: 0,
-  input: "baaa",
-  o: console.log.bind(console),
-};
-const g = rna(mn`
-S → A B | B A
-    A → a a
-    B → a b`);
-console.log(g.toString());
-parse_pith(
-  { input: "aaab", pos: 0, o: console.log.bind(console) },
-  "A",
-  g(),
-  null
-);
+//const gtxt = `S → A B C
+//A → a
+//B → b
+//C → c`
+//// S → C
+//// C → C b | C a | a_ | c_ | b_
+//const dna = mn([gtxt])
+//const g = rna(dna);
+//// seen output path head input
+//// SOPHI
+//console.log(g.toString());
+//const S = g() 
+//S(
+//  {
+//    head: [[0, "A", null], null],
+//    tail: [null, null],
+//    input: "abc",
+//    o: console.log.bind(console),
+//  },
+//  parse_pith
+//);
 function parse_pith(c, m, h, t = null) {
-  if ((c.so = (c.so || 0) + 1) > 39) return c.o(-19, "stackoverflow");
-  c.o(("P" + m + ">").padStart(5, " "), c);
+  if ((c.so = (c.so || 0) + 1) > 39) return c.o(-39, "stackoverflow");
+  c.o((c.so+'').padStart(2,"0")+("P" + m + ">").padStart(5, " ") + configToString(c));
   match(m, {
     A() {
-      h({ ...c, vh: [h, c.vh], vt: [t, c.vt], tail: c }, parse_pith);
+      c.head = [[c.head[0][0], "A", h], c.head];
+      c.tail = [t, c.tail];
+      h(c, parse_pith);
     },
     T() {
-      let len = match_term(h, c.input, c.pos);
+      let len = match_term(h, c.input, c.head[0][0]);
       if (len < 0) {
-        throw new Error('na')
+        while (c.head[0][1] !== "A") c.head = c.head[1];
         t(c, find_next_production_pith);
       } else {
-        t(
-          { ...c, stack: [["T", h, len], c.stack], pos: c.pos + len, tail: c },
-          parse_pith
-        );
+        const cpos = c.head[0][0];
+        const npos = cpos + len;
+        const term = c.input.slice(cpos, npos);
+        c.head = [[npos, "T", term, h], c.head];
+        t(c, parse_pith);
       }
     },
     G() {
-      const terms = c.stack.shift();
-      const lhs = terms.shift();
-      let start = lhs[2];
-      const r = h(
-        ...terms.map(([m, t, l], i) =>
-          match(m, {
-            U: () => ((start += l), t),
-            T: () => c.input.slice(start, (start += l)),
-          })
-        )
-      );
-      const u = ["U", r, start - lhs[2], lhs[1]];
-      if (c.stack.length) c.stack[0].push(u), c.tails.shift()(c, parse_pith);
-      else if (c.tails[0] === null) c.o(u);
-      else c.o(-1);
+      const args = [];
+      const npos = c.head[0][0];
+      while (c.head[0][1] !== "A") {
+        args.unshift(c.head[0][2]);
+        c.head = c.head[1];
+      }
+      const u = [npos, "U", h(...args), c.head[0][2]];
+      c.head = c.head[1];
+      c.head = [u, c.head];
+      const ct = c.tail[0];
+      c.tail = c.tail[1];
+      if (ct) ct(c, parse_pith);
+      else c.o(c.head[0]);
     },
   });
 }
-function skip_matched_terms_pith(c, m, h, t) {
-  c.o(("SMT" + m + ">").padStart(5, " ") + configToString(c));
-  parse_pith(c, m, h, t);
-}
 function find_next_production_pith(c, m, h, t) {
-  c.o(("FNP" + m + ">").padStart(5, " ") + configToString(c));
   match(m, {
     G() {
-      if (t) return t(c, skip_matched_terms_pith);
-      c.pos = c.stack.shift()[0][2];
-      if (c.tails[0] === null) return c.o(-1);
-      c.tails.shift()(c, find_next_production_pith);
+      if (t) return t(c, parse_pith);
+      t = c.tail[0];
+      c.head = c.head[1];
+      c.tail = c.tail[1];
+      if (t) t(c, find_next_production_pith);
+      else c.o(-1);
     },
     default() {
       t(c, find_next_production_pith);
@@ -208,18 +216,20 @@ function match_term(str, input, offset) {
   if (i === l) return l;
   return -1;
 }
-
+function toArray(l){
+  const a = []
+  while(l){
+    a.push(l[0])
+    l = l[1]
+  }
+  return a
+}
 function configToString(c) {
-  const cols = [36, 9, 63];
-  const s = subscript(
-    [...c.stack]
-      .reverse()
-      .map((a) => a.map((a) => a[0] + nameof(a[1])).join(""))
-      .join(" ")
-  );
-  const t = subscript(c.tails.map((t) => nameof(t)).join(" "));
-  const i = c.input.slice(c.pos);
-  const p = c.pos.toString().padStart(2, " ");
+  const cols = [12, 9, 12];
+  const s = subscript(toArray(c.head).reverse().map(h =>  nameof(h[1])+nameof(h[2])).join(';'));
+  const t = subscript(toArray(c.tail).map(t => nameof(t)).join(';'));
+  const i = c.input.slice(c.head[0][0]);
+  const p = c.head[0][0].toString().padStart(2, " ");
 
   //prettier-ignore
   return `${s.padStart(cols[0], " ").slice(-cols[0])} ${p} ${
@@ -256,7 +266,7 @@ function subscript(x) {
 function tdd(tests) {
   const tasks = [];
   for (let key in tests) {
-    const gr = programmer([key]);
+    const gr = rna(mn([key]));
     const g = gr((...a) => a.join(""));
     for (let input of tests[key]) tasks.push([g, input, gr.toString()]);
   }
@@ -265,12 +275,12 @@ function tdd(tests) {
     const t = tasks.shift();
     if (!t) return;
     const logs = [];
-    const c = { stack: [], tails: [], pos: 0, input: t[1], o };
+    const c = { head: [[0, "A"], null], tail: [null, null], input: t[1], o };
     var gotrez = false;
     function o(rez) {
       if (typeof rez === "string") return logs.push(rez);
       gotrez = true;
-      if (rez < 0 || rez[1] !== t[1]) {
+      if (rez < 0 || rez[2] !== t[1]) {
         console.log(t[1]);
         console.log(t[2]);
         console.log(logs.join("\n"));
@@ -279,7 +289,7 @@ function tdd(tests) {
       setImmediate(next);
     }
     try {
-      parse_pith(c, "A", t[0], null);
+      t[0](c, parse_pith);
       if (!gotrez) console.log("dont have rez");
     } catch (e) {
       console.log(e);
