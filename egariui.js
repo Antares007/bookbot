68e53d629172a876df8afe5574bf8696d021637e
Archil Bolkvadze <a.bolkvadze@gmail.com>
2021-03-04 13:45:29 +0400

    add event handling

--- egariui.js
+++ egariui.js
@@ -1,10 +1,43 @@
-function makePith(elm, depth=0) {
-  var childs_count = 0;
+const atack = [];
+const A = (...args) => atack.push(args);
+const S = () => atack.pop();
+const nand = (a) => (b) => (o) => a(() => b(o));
+const nor = (a) => (b) => (o) => {
+  var f = true;
+  a(() => {
+    f = false;
+    o();
+  });
+  if (f) b(o);
+};
+const counter = (o) => {
+  const [d] = S();
+  A("element", "button", (o) => {
+    A("on", "click", (e) => {
+      console.log(e.target);
+    }),
+      o();
+    A("text", "+"), o();
+    if (d) A(d - 1), counter(o);
+  }),
+    o();
+  A("element", "button", (o) => {
+    A("text", "-"), o();
+    if (d) A(d - 1), counter(o);
+  }),
+    o();
+  A("text", "0"), o();
+};
+const makePith = (elm) => {
   const { childNodes } = elm;
   const childPiths = [];
-  return function pith(t, ...args) {
-    if (t === "element") {
-      const [tag, onar, key] = args;
+  var childs_count = 0;
+  const listeners = [];
+  var listeners_count = 0;
+  return function pith() {
+    const [m, ...rest] = S();
+    if (m === "element") {
+      const [tag, onar, key] = rest;
       let n, ob;
       const index = childs_count++;
       const TAG = tag.toUpperCase();
@@ -20,18 +53,18 @@ function makePith(elm, depth=0) {
               childPiths.splice(index, 0, ...childPiths.splice(i, 1));
 
           if ((ob = childPiths[index]))
-            if (key == null) return onar(ob), ob();
+            if (key == null) return onar(ob), A(), ob();
             else return;
-          childPiths.splice(index, 0, (ob = makePith( n, depth + 1)));
-          return onar(ob), ob();
+          childPiths.splice(index, 0, (ob = makePith(n)));
+          return onar(ob), A(), ob();
         }
       n = document.createElement(TAG);
       if (key != null) n.setAttribute("key", key);
       elm.insertBefore(n, childNodes[index]),
-        childPiths.splice(index, 0, (ob = makePith( n, depth + 1)));
-      onar(ob), ob();
-    } else if (t === "text") {
-      const [text] = args;
+        childPiths.splice(index, 0, (ob = makePith(n)));
+      onar(ob), A(), ob();
+    } else if (m === "text") {
+      const [text] = rest;
       const index = childs_count++;
       for (let i = index, l = childNodes.length; i < l; i++)
         if (
@@ -45,17 +78,40 @@ function makePith(elm, depth=0) {
         }
       elm.insertBefore(document.createTextNode(text), childNodes[index]),
         childPiths.splice(index, 0, null);
-    } else if (t === "get") {
-      args[0](elm);
-    } else if (t === void 0) {
+    } else if (m === "on") {
+      const index = listeners_count++;
+      for (let i = index, l = listeners.length; i < l; i++)
+        if (
+          listeners[i][0] === rest[0] &&
+          listeners[i][1] === rest[1] &&
+          listeners[i][2] === rest[2]
+        ) {
+          if (index < i) listeners.splice(index, 0, ...listeners.splice(i, 1));
+          return;
+        }
+      elm.addEventListener(...rest), listeners.splice(index, 0, rest);
+    } else if (m === void 0) {
       for (let l = childNodes.length; l > childs_count; l--)
         elm.removeChild(childNodes[childs_count]);
-      const piths = childPiths.splice(
+      const oldChildPiths = childPiths.splice(
         childs_count,
         childPiths.length - childs_count
       );
       childs_count = 0;
-      for (let mp of piths) mp && mp();
+      for (let ocp of oldChildPiths) ocp && (A(), ocp());
+      const oldListeners = listeners.splice(
+        listeners_count,
+        listeners.length - listeners_count
+      );
+      listeners_count = 0;
+      for (let ol of oldListeners) elm.removeEventListener(...ol);
+    } else {
+      //console.error("na");
     }
   };
-}
+};
+const body = (document.body = document.createElement("body"));
+const ob = makePith(body);
+A(2), counter(ob);
+A(), ob();
+Object.assign(window, { A, S, o: ob, C: counter, atack });
